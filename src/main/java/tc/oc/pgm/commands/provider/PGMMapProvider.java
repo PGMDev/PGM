7aa898620ffcc95797f6402bc3b3f2f23349ebfa
Jorge Orlando <bgmp00@protonmail.com>
2019-12-17 19:40:35 -0300

    Allow the return of a null next map on PGMMapProvider
    Co-Authored-By: Pablo Herrera <pabloherrerapalacio@gmail.com>
    Signed-off-by: BGM <bgmp00@protonmail.com>

--- src/main/java/tc/oc/pgm/commands/provider/PGMMapProvider.java
+++ src/main/java/tc/oc/pgm/commands/provider/PGMMapProvider.java
@@ -47,7 +47,7 @@ public class PGMMapProvider implements BukkitProvider<PGMMap> {
       map = matchManager.getMapOrder().getNextMap();
     }
 
-    if (map == null) {
+     if (map == null && !isGoToNext(annotations)) {
       throw new ArgumentException(AllTranslations.get().translate("command.mapNotFound", sender));
     }
 
