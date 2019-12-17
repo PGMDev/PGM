852374b7359fad51ae7d966fbb5d45f1f5922ae0
BGM <bgmp00@protonmail.com>
2019-12-17 03:14:39 -0300

    Fix typo
    Signed-off-by: BGM <bgmp00@protonmail.com>

--- src/main/java/tc/oc/pgm/rotation/PGMMapOrder.java
+++ src/main/java/tc/oc/pgm/rotation/PGMMapOrder.java
@@ -3,7 +3,7 @@ package tc.oc.pgm.rotation;
 import tc.oc.pgm.map.PGMMap;
 
 /**
- * An order of {@link PGMMap}s, which can either be a @{@link FixedPGMMapOrder} or a {@link
+ * An order of {@link PGMMap}s, which can either be a {@link FixedPGMMapOrder} or a {@link
  * RandomPGMMapOrder}
  */
 public interface PGMMapOrder {
