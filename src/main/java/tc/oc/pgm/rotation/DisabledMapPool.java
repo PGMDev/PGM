de7f67702311ec37bd380027733d6afac6325f2a
Pablete1234 <pabloherrerapalacio@gmail.com>
2019-12-26 08:04:39 +0100

    Add no-op PGMMapOrder itf to DisabledMapPool
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- src/main/java/tc/oc/pgm/rotation/DisabledMapPool.java
+++ src/main/java/tc/oc/pgm/rotation/DisabledMapPool.java
@@ -1,14 +1,25 @@
 package tc.oc.pgm.rotation;
 
 import org.bukkit.configuration.ConfigurationSection;
+import tc.oc.pgm.map.PGMMap;
 
 public class DisabledMapPool extends MapPool {
-  public DisabledMapPool(MapPoolManager manager, ConfigurationSection section, String name) {
+  DisabledMapPool(MapPoolManager manager, ConfigurationSection section, String name) {
     super(manager, section, name);
   }
 
   @Override
   public boolean isEnabled() {
     return false;
+  }
+
+  @Override
+  public PGMMap popNextMap() {
+    return getRandom();
+  }
+
+  @Override
+  public PGMMap getNextMap() {
+    return null;
   }
 }
