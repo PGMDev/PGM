411fdfb5c0dace60416df71b2b66c2bdd0918bcf
Ashcon Partovi <ashcon@partovi.net>
2019-12-27 12:16:23 -0700

    Add a death message setting, defaults to own
    Signed-off-by: Ashcon Partovi <ashcon@partovi.net>

--- src/main/java/tc/oc/pgm/death/DeathMessageMatchModule.java
+++ src/main/java/tc/oc/pgm/death/DeathMessageMatchModule.java
@@ -1,6 +1,5 @@
 package tc.oc.pgm.death;
 
-import java.util.UUID;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,10 +8,12 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import tc.oc.component.Component;
 import tc.oc.component.types.PersonalizedText;
 import tc.oc.component.types.PersonalizedTranslatable;
+import tc.oc.pgm.api.Permissions;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.match.MatchScope;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.api.player.event.MatchPlayerDeathEvent;
+import tc.oc.pgm.api.setting.SettingKey;
 import tc.oc.pgm.events.ListenerScope;
 import tc.oc.pgm.match.MatchModule;
 import tc.oc.pgm.match.MatchModuleFactory;
@@ -52,14 +53,22 @@ public class DeathMessageMatchModule extends MatchModule implements Listener {
           new PersonalizedText(" "), new PersonalizedTranslatable("death.predictedSuffix"));
     }
 
-    UUID victimId = event.getVictim().getId();
-    UUID killerId = event.getKiller() == null ? null : event.getKiller().getId();
-
     for (MatchPlayer viewer : event.getMatch().getPlayers()) {
-      if (event.isInvolved(viewer)) {
-        viewer.sendMessage(new PersonalizedText(message, ChatColor.BOLD));
-      } else {
-        viewer.sendMessage(message);
+      switch (viewer.getSettings().getValue(SettingKey.DEATH)) {
+        case DEATH_OWN:
+          if (event.isInvolved(viewer)) {
+            viewer.sendMessage(message);
+          } else if (event.isTeamKill() && viewer.getBukkit().hasPermission(Permissions.STAFF)) {
+            viewer.sendMessage(new PersonalizedText(message, ChatColor.ITALIC));
+          }
+          break;
+        case DEATH_ALL:
+          if (event.isInvolved(viewer)) {
+            viewer.sendMessage(new PersonalizedText(message, ChatColor.BOLD));
+          } else {
+            viewer.sendMessage(message);
+          }
+          break;
       }
     }
   }
