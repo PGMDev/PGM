a3db0124601ee75285f5ea55966065745a14d844
Pugzy <pugzy@mail.com>
2020-01-02 21:59:38 +0000

    Add basic report command with alert and event
    Signed-off-by: Pugzy <pugzy@mail.com>

---
+++ src/main/java/tc/oc/pgm/events/PlayerReportEvent.java
@@ -0,0 +1,45 @@
+package tc.oc.pgm.events;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.HandlerList;
+import tc.oc.pgm.api.event.ExtendedCancellable;
+import tc.oc.pgm.api.player.MatchPlayer;
+
+/** Called immediately AFTER a player runs the report command. */
+public class PlayerReportEvent extends ExtendedCancellable {
+
+  private final CommandSender sender;
+  private final MatchPlayer player;
+  private final String reason;
+
+  public PlayerReportEvent(CommandSender sender, MatchPlayer player, String reason) {
+    this.sender = checkNotNull(sender);
+    this.player = checkNotNull(player);
+    this.reason = checkNotNull(reason);
+  }
+
+  public CommandSender getSender() {
+    return sender;
+  }
+
+  public MatchPlayer getPlayer() {
+    return player;
+  }
+
+  public String getReason() {
+    return reason;
+  }
+
+  private static final HandlerList handlers = new HandlerList();
+
+  @Override
+  public HandlerList getHandlers() {
+    return handlers;
+  }
+
+  public static HandlerList getHandlerList() {
+    return handlers;
+  }
+}
