3e174f42803b48ad5e20d965711e843f9765b8f7
TheMolkaPL <themolkapl@gmail.com>
2020-01-02 15:47:28 +0100

    Maptag command parsing improvements
    Signed-off-by: TheMolkaPL <themolkapl@gmail.com>

--- src/main/java/tc/oc/pgm/commands/provider/MapTagsConditionProvider.java
+++ src/main/java/tc/oc/pgm/commands/provider/MapTagsConditionProvider.java
@@ -8,18 +8,21 @@ import app.ashcon.intake.argument.Namespace;
 import app.ashcon.intake.parametric.Provider;
 import app.ashcon.intake.parametric.ProvisionException;
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.Set;
 import javax.annotation.Nullable;
 import tc.oc.pgm.map.MapLibrary;
 import tc.oc.pgm.map.PGMMap;
 import tc.oc.pgm.maptag.MapTag;
-import tc.oc.pgm.maptag.MapTagSet;
 import tc.oc.pgm.maptag.MapTagsCondition;
+import tc.oc.util.Pair;
 
 public class MapTagsConditionProvider implements Provider<MapTagsCondition> {
 
@@ -43,22 +46,13 @@ public class MapTagsConditionProvider implements Provider<MapTagsCondition> {
       throws ArgumentException, ProvisionException {
     Map<MapTag, Boolean> mapTags = new HashMap<>();
     while (args.hasNext()) {
-      String query = args.next().toLowerCase(Locale.ROOT);
-      boolean allow = true;
-
-      if (query.startsWith(NEGATION)) {
-        query = query.substring(NEGATION.length());
-        allow = false;
-      }
-
-      if (!query.startsWith(PREFIX)) {
-        continue;
-      }
-      query = query.substring(PREFIX.length());
-
-      if (!query.isEmpty()) {
-        mapTags.put(MapTag.forName(query), allow);
-      }
+      parseMapTag(args.next())
+          .ifPresent(
+              pair -> {
+                if (!pair.first.isEmpty()) {
+                  mapTags.put(MapTag.forName(pair.first), pair.second);
+                }
+              });
     }
 
     return new MapTagsCondition(mapTags);
@@ -67,21 +61,47 @@ public class MapTagsConditionProvider implements Provider<MapTagsCondition> {
   @Override
   public List<String> getSuggestions(
       String prefix, Namespace namespace, List<? extends Annotation> modifiers) {
-    String query = prefix.toLowerCase(Locale.ROOT);
+    return parseMapTag(prefix)
+        .map(
+            pair -> {
+              Set<String> mapTags = new HashSet<>();
+              for (PGMMap map : mapLibrary.getMaps()) {
+                for (MapTag mapTag : map.getPersistentContext().getMapTags()) {
+                  if (mapTag.getName().startsWith(pair.first)) {
+                    mapTags.add(mapTagToString(mapTag, pair.second));
+                  }
+                }
+              }
+
+              return (List<String>) new ArrayList<>(mapTags);
+            })
+        .orElse(Collections.emptyList());
+  }
+
+  private Optional<Pair<String, Boolean>> parseMapTag(String query) {
+    checkNotNull(query);
+    query = query.toLowerCase(Locale.ROOT);
+    boolean allow = true;
+
+    if (query.startsWith(NEGATION)) {
+      query = query.substring(NEGATION.length());
+      allow = false;
+    }
+
     if (!query.startsWith(PREFIX)) {
-      return Collections.emptyList();
+      return Optional.empty();
     }
+
     query = query.substring(PREFIX.length());
+    return Optional.of(Pair.create(query, allow));
+  }
 
-    MapTagSet mapTags = MapTagSet.mutable();
-    for (PGMMap map : mapLibrary.getMaps()) {
-      for (MapTag mapTag : map.getPersistentContext().getMapTags()) {
-        if (mapTag.getName().startsWith(query)) {
-          mapTags.add(mapTag);
-        }
-      }
+  private String mapTagToString(MapTag mapTag, boolean allow) {
+    StringBuilder toString = new StringBuilder();
+    if (!allow) {
+      toString.append(NEGATION);
     }
-
-    return mapTags.stream().map(MapTag::toString).collect(Collectors.toList());
+    toString.append(mapTag.toString());
+    return toString.toString();
   }
 }
