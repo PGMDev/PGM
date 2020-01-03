1a604f129d510489aa9ec0a398a31b3c787c139d
TheMolkaPL <themolkapl@gmail.com>
2020-01-04 00:56:39 +0100

    Make SettingKey provider more friendly
    Signed-off-by: TheMolkaPL <themolkapl@gmail.com>

--- src/main/java/tc/oc/pgm/api/setting/SettingKey.java
+++ src/main/java/tc/oc/pgm/api/setting/SettingKey.java
@@ -1,6 +1,12 @@
 package tc.oc.pgm.api.setting;
 
+import static com.google.common.base.Preconditions.*;
 import static tc.oc.pgm.api.setting.SettingValue.*;
+
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A toggleable setting with various possible {@link SettingValue}s.
@@ -9,14 +15,20 @@ import static tc.oc.pgm.api.setting.SettingValue.*;
  */
 public enum SettingKey {
   CHAT("chat", CHAT_TEAM, CHAT_GLOBAL, CHAT_ADMIN), // Changes the default chat channel
-  DEATH("death", DEATH_OWN, DEATH_ALL), // Changes which death messages are seen
+  DEATH(
+      Arrays.asList("death", "dms"), DEATH_ALL, DEATH_OWN), // Changes which death messages are seen
   PICKER("picker", PICKER_AUTO, PICKER_ON, PICKER_OFF); // Changes when the picker is displayed
 
-  private final String name;
+  private final List<String> aliases;
   private final SettingValue[] values;
 
   SettingKey(String name, SettingValue... values) {
-    this.name = name;
+    this(Collections.singletonList(name), values);
+  }
+
+  SettingKey(List<String> aliases, SettingValue... values) {
+    checkArgument(!aliases.isEmpty(), "aliases is empty");
+    this.aliases = ImmutableList.copyOf(aliases);
     this.values = values;
   }
 
@@ -26,7 +38,16 @@ public enum SettingKey {
    * @return The name.
    */
   public String getName() {
-    return name;
+    return aliases.get(0);
+  }
+
+  /**
+   * Get all aliases of this {@link SettingKey}.
+   *
+   * @return An immutable list of all aliases.
+   */
+  public List<String> getAliases() {
+    return aliases;
   }
 
   /**
