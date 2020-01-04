ec8274f1ee6849a015069ab5587ed459545c48ff
TheMolkaPL <themolkapl@gmail.com>
2020-01-04 23:10:23 +0100

    Rename shown/hidden to on/off
    Signed-off-by: TheMolkaPL <themolkapl@gmail.com>

--- src/main/java/tc/oc/pgm/match/MatchPlayerImpl.java
+++ src/main/java/tc/oc/pgm/match/MatchPlayerImpl.java
@@ -176,7 +176,7 @@ public class MatchPlayerImpl implements MatchPlayer, MultiAudience, Comparable<M
   public boolean canSee(MatchPlayer other) {
     if (!other.isVisible()) return false;
     if (other.isParticipating()) return true;
-    return getSettings().getValue(SettingKey.OBSERVERS) == SettingValue.OBSERVERS_SHOWN;
+    return getSettings().getValue(SettingKey.OBSERVERS) == SettingValue.OBSERVERS_ON;
   }
 
   @Override
