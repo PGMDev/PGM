8443f7412933d8161fa7a67979950d1ea3020399
Meeples10 <8867705+Meeples10@users.noreply.github.com>
2020-01-18 22:32:04 -0500

    Refactor ViaUtils to use reflection
    Signed-off-by: Meeples10 <8867705+Meeples10@users.noreply.github.com>

--- src/main/java/tc/oc/util/ViaUtils.java
+++ src/main/java/tc/oc/util/ViaUtils.java
@@ -1,16 +1,48 @@
 package tc.oc.util;
 
-import org.bukkit.Bukkit;
+import java.lang.reflect.Method;
 import org.bukkit.entity.Player;
+import tc.oc.pgm.api.PGM;
 import tc.oc.world.NMSHacks;
-import us.myles.ViaVersion.api.Via;
 
 public class ViaUtils {
-  public static boolean enabled() {
-    return Bukkit.getServer().getPluginManager().getPlugin("ViaVersion") != null;
+  private static boolean enabled;
+  private static Object viaAPI;
+  private static Method getPlayerVersion;
+
+  static {
+    try {
+      viaAPI = Class.forName("us.myles.ViaVersion.api.Via").getMethod("getAPI").invoke(null);
+      getPlayerVersion =
+          Class.forName("us.myles.ViaVersion.api.ViaAPI")
+              .getMethod("getPlayerVersion", Object.class);
+      enabled = true;
+    } catch (Exception e) {
+      PGM.get().getLogger().warning("ViaVersion is not installed");
+      e.printStackTrace();
+      enabled = false;
+    }
   }
 
+  public static boolean enabled() {
+    return enabled;
+  }
+
+  /**
+   * @see <a
+   *     href="https://wiki.vg/Protocol_version_numbers">https://wiki.vg/Protocol_version_numbers</a>
+   */
   public static int getProtocolVersion(Player player) {
-    return enabled() ? Via.getAPI().getPlayerVersion(player) : NMSHacks.getProtocolVersion(player);
+    int version = NMSHacks.getProtocolVersion(player);
+    if (enabled()) {
+      try {
+        return (int) getPlayerVersion.invoke(viaAPI, player);
+      } catch (Exception e) {
+        e.printStackTrace();
+        return version;
+      }
+    } else {
+      return version;
+    }
   }
 }
