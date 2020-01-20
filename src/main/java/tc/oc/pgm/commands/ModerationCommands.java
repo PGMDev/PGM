3f3ce548027f37a875b1686fcb0be78cb9bbf8f9
AppleNick <applenick@users.noreply.github.com>
2020-01-20 15:19:52 -0800

    Add a cooldown to /report, resolves Electroid#242

--- src/main/java/tc/oc/pgm/commands/ModerationCommands.java
+++ src/main/java/tc/oc/pgm/commands/ModerationCommands.java
@@ -1,7 +1,14 @@
 package tc.oc.pgm.commands;
 
 import app.ashcon.intake.Command;
+import app.ashcon.intake.CommandException;
 import app.ashcon.intake.parametric.annotation.Text;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
@@ -18,6 +25,11 @@ import tc.oc.pgm.events.PlayerReportEvent;
 
 public class ModerationCommands {
 
+  private static final int REPORT_COOLDOWN_SECONDS = 15;
+
+  private static final Cache<UUID, Instant> LAST_REPORT_SENT =
+      CacheBuilder.newBuilder().expireAfterWrite(REPORT_COOLDOWN_SECONDS, TimeUnit.SECONDS).build();
+
   @Command(
       aliases = {"report"},
       usage = "<player> <reason>",
@@ -27,7 +39,36 @@ public class ModerationCommands {
       MatchPlayer matchPlayer,
       Match match,
       Player player,
-      @Text String reason) {
+      @Text String reason)
+      throws CommandException {
+    if (!commandSender.hasPermission(Permissions.STAFF) && commandSender instanceof Player) {
+      // Check for cooldown
+      Instant lastReport = LAST_REPORT_SENT.getIfPresent(matchPlayer.getId());
+      if (lastReport != null) {
+        Duration timeSinceReport = Duration.between(lastReport, Instant.now());
+        long secondsRemaining = REPORT_COOLDOWN_SECONDS - timeSinceReport.getSeconds();
+        if (secondsRemaining > 0) {
+          Component secondsComponent = new PersonalizedText(Long.toString(secondsRemaining));
+          Component secondsLeftComponent =
+              new PersonalizedTranslatable(
+                      secondsRemaining != 1
+                          ? "countdown.pluralCompound"
+                          : "countdown.singularCompound",
+                      secondsComponent)
+                  .getPersonalizedText()
+                  .color(ChatColor.AQUA);
+          commandSender.sendMessage(
+              new PersonalizedTranslatable("command.cooldown", secondsLeftComponent)
+                  .getPersonalizedText()
+                  .color(ChatColor.RED));
+          return;
+        }
+      } else {
+        // Player has no cooldown, so add one
+        LAST_REPORT_SENT.put(matchPlayer.getId(), Instant.now());
+      }
+    }
+
     MatchPlayer accused = match.getPlayer(player);
     PlayerReportEvent event = new PlayerReportEvent(commandSender, accused, reason);
     match.callEvent(event);
