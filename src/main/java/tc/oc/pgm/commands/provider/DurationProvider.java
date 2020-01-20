33d3224bdc17dc92b708e05c0c8ddd9e65fa7cd3
KingOfSquares <simonmorland@gmail.com>
2020-01-21 00:35:17 +0100

    Make commands using duration read config

--- src/main/java/tc/oc/pgm/commands/provider/DurationProvider.java
+++ src/main/java/tc/oc/pgm/commands/provider/DurationProvider.java
@@ -27,6 +27,6 @@ public class DurationProvider implements BukkitProvider<Duration> {
           .toStandardDuration()
           .toDuration();
     }
-    return Duration.standardSeconds(30L);
+    return null;
   }
 }
