df09e973d74aef22b5eeb2c24db8f9058b8cac26
applenick <applenick@users.noreply.github.com>
2020-01-26 23:01:46 -0800

    Fix formatting error
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/pgm/api/player/MatchPlayer.java
+++ src/main/java/tc/oc/pgm/api/player/MatchPlayer.java
@@ -202,7 +202,7 @@ public interface MatchPlayer extends Audience, Named, Tickable, InventoryHolder 
    */
   void setGameMode(GameMode gameMode);
 
-  /** 
+  /**
    * Get the protocol version of the {@link MatchPlayer}'s client
    *
    * @return The protocol version of the {@link MatchPlayer}'s client
