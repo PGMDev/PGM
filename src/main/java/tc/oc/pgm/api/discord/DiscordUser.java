e94aa0daff80fad4c92d8ef09ff92d4c071e3064
Vlad <me@vladyslav.xyz>
2020-02-01 11:06:25 +0200

    Add DiscordUser interface
    Signed-off-by: Vlad <me@vladyslav.xyz>

--- src/main/java/tc/oc/pgm/api/discord/DiscordUser.java
+++ src/main/java/tc/oc/pgm/api/discord/DiscordUser.java
@@ -3,13 +3,13 @@ package tc.oc.pgm.api.discord;
 import jdk.internal.jline.internal.Nullable;
 
 public interface DiscordUser {
-    // Gets the Discord username of the user, or null if not registered
-    @Nullable
-    String getUsername();
+  // Gets the Discord username of the user, or null if not registered
+  @Nullable
+  String getUsername();
 
-    // Sets the Discord username
-    void setUsername(String username);
+  // Sets the Discord username
+  void setUsername(String username);
 
-    // Gets verification token for a user, cached or un-cached
-    String getToken();
+  // Gets verification token for a user, cached or un-cached
+  String getToken();
 }
