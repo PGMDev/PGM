db331984b777d5cddc60696eb900ba0541a274aa
Indicado <indicado@walrus.gg>
2020-05-02 22:22:46 +0200

    Migrate Information and Environment sections of Modules
    Signed-off-by: Indicado <indicado@walrus.gg>

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,13 @@
 module.exports = {
   Modules: {
     General: ["modules/main", "modules/includes-conditionals"],
-    Information: ["modules/tutorial"],
+    Information: ["modules/tutorial", "modules/broadcasts", "modules/rules"],
+    Environment: [
+      "modules/world",
+      "modules/time",
+      "modules/border",
+      "modules/mobs",
+    ],
   },
   Reference: {
     Items: ["reference/inventory"],
