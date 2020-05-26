2992a4f792ee5db5a45173574b851977ed9ae43b
KingOfSquares <simonmorland@gmail.com>
2020-05-26 12:18:28 +0200

    format and rebase
    Signed-off-by: KingOfSquares <simonmorland@gmail.com>

--- core/src/main/java/tc/oc/pgm/modules/MapmakerMatchModule.java
+++ core/src/main/java/tc/oc/pgm/modules/MapmakerMatchModule.java
@@ -1,6 +1,6 @@
 package tc.oc.pgm.modules;
 
-import org.bukkit.entity.Player;
+import java.util.Collection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import tc.oc.pgm.api.PGM;
@@ -8,27 +8,27 @@ import tc.oc.pgm.api.map.Contributor;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.match.MatchModule;
 import tc.oc.pgm.api.match.MatchScope;
-import tc.oc.pgm.api.match.event.MatchUnloadEvent;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.api.player.event.MatchPlayerAddEvent;
 import tc.oc.pgm.events.ListenerScope;
 
-import java.util.Collection;
-
 @ListenerScope(MatchScope.LOADED)
 public class MapmakerMatchModule implements MatchModule, Listener {
-    private final Collection<Contributor> authors;
+  private final Collection<Contributor> authors;
 
-    public MapmakerMatchModule(Match match) {
-        this.authors = match.getMap().getAuthors();
-    }
+  public MapmakerMatchModule(Match match) {
+    this.authors = match.getMap().getAuthors();
+  }
 
-    @EventHandler(ignoreCancelled = true)
-    public void onPlayerAddEvent(final MatchPlayerAddEvent event) {
-        MatchPlayer player = event.getPlayer();
-        player.getBukkit().addAttachment(PGM.get(), "pgm.group.mapmaker", authors.stream().anyMatch(c -> c.isPlayer(player.getId())));
-        PGM.get().getPrefixRegistry().removePlayer(player.getId()); //Refresh prefixes for the player
-    }
-
-
+  @EventHandler(ignoreCancelled = true)
+  public void onPlayerAddEvent(final MatchPlayerAddEvent event) {
+    MatchPlayer player = event.getPlayer();
+    player
+        .getBukkit()
+        .addAttachment(
+            PGM.get(),
+            "pgm.group.mapmaker",
+            authors.stream().anyMatch(c -> c.isPlayer(player.getId())));
+    PGM.get().getPrefixRegistry().removePlayer(player.getId()); // Refresh prefixes for the player
+  }
 }
