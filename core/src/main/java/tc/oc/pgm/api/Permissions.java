c905c2a481263e520a9efb113519a1734617456a
Jacob Bearden <jacob@bearden.io>
2020-05-27 22:47:05 -0600

    make extra voting an actual permission node
    Signed-off-by: Jacob Bearden <jacob@bearden.io>

--- core/src/main/java/tc/oc/pgm/api/Permissions.java
+++ core/src/main/java/tc/oc/pgm/api/Permissions.java
@@ -23,6 +23,7 @@ public interface Permissions {
   String RESIZE = ROOT + ".resize"; // Resize the number of players per match
   String JOIN = ROOT + ".join"; // Allowed to join a match as a participant
   String JOIN_CHOOSE = JOIN + ".choose"; // Can choose which team to join
+  String EXTRA_VOTE = JOIN + ".extravote"; // Extra map voting power
   String JOIN_FULL = ROOT + ".full"; // Can join a team or server if it is full
   String JOIN_FORCE = JOIN + ".force"; // Can force other players onto teams
   String LEAVE = ROOT + ".leave"; // Can join observers willingly
@@ -53,6 +54,7 @@ public interface Permissions {
               .put(DEFAULT.getName(), true)
               .put(JOIN_CHOOSE, true)
               .put(JOIN_FULL, true)
+              .put(EXTRA_VOTE, true)
               .build());
 
   Permission MODERATOR =
