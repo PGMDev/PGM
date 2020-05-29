b32800a6536e870012f4832fa9a23d3f77e88cdd
applenick <applenick@users.noreply.github.com>
2020-05-29 10:57:47 -0700

    Add a broadcast to /cancelrestart
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/command/CancelCommand.java
+++ core/src/main/java/tc/oc/pgm/command/CancelCommand.java
@@ -3,13 +3,16 @@ package tc.oc.pgm.command;
 import app.ashcon.intake.Command;
 import net.kyori.text.TranslatableComponent;
 import net.kyori.text.format.TextColor;
+import org.bukkit.command.CommandSender;
 import tc.oc.pgm.api.Permissions;
 import tc.oc.pgm.api.match.Match;
+import tc.oc.pgm.listeners.ChatDispatcher;
 import tc.oc.pgm.restart.CancelRestartEvent;
 import tc.oc.pgm.restart.RestartManager;
 import tc.oc.pgm.start.StartMatchModule;
 import tc.oc.pgm.timelimit.TimeLimitCountdown;
 import tc.oc.pgm.timelimit.TimeLimitMatchModule;
+import tc.oc.pgm.util.UsernameFormatUtils;
 import tc.oc.pgm.util.chat.Audience;
 
 public final class CancelCommand {
@@ -18,11 +21,15 @@ public final class CancelCommand {
       aliases = {"cancel", "cancelrestart", "cr"},
       desc = "Cancels all countdowns",
       perms = Permissions.STOP)
-  public void cancel(Audience audience, Match match) {
+  public void cancel(Audience audience, CommandSender sender, Match match) {
     if (RestartManager.isQueued()) {
       match.callEvent(new CancelRestartEvent());
       audience.sendMessage(
           TranslatableComponent.of("admin.cancelRestart.restartUnqueued", TextColor.RED));
+      ChatDispatcher.broadcastAdminChatMessage(
+          TranslatableComponent.of("admin.cancelRestart.broadcast", TextColor.GRAY)
+              .args(UsernameFormatUtils.formatStaffName(sender, match)),
+          match);
       return;
     }
 
