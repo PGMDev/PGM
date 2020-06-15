0f27532ce48ae3d6f7a8c6c368eb57b41b4a00af
applenick <applenick@users.noreply.github.com>
2020-06-15 15:10:34 -0700

    Use component builders in PlayerComponent
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- util/src/main/java/tc/oc/pgm/util/text/types/PlayerComponent.java
+++ util/src/main/java/tc/oc/pgm/util/text/types/PlayerComponent.java
@@ -23,40 +23,6 @@ public interface PlayerComponent {
   static Component UNKNOWN =
       TranslatableComponent.of("misc.unknown", TextColor.DARK_AQUA, TextDecoration.ITALIC);
 
-  static Component of(Player player, NameStyle style) {
-    return of(player, "", style);
-  }
-
-  static Component of(@Nullable Player player, String defName, NameStyle style) {
-
-    // Offline player or not visible
-    if ((player == null || !player.isOnline())) {
-      return formatOffline(defName, style == NameStyle.PLAIN);
-    }
-
-    // For name styles that don't allow vanished, make vanished appear offline
-    if (!style.showVanish && isVanished(player)) {
-      return formatOffline(player.getName(), style == NameStyle.PLAIN);
-    }
-
-    switch (style) {
-      case COLOR:
-        return formatTeleport(formatColor(player), player.getName());
-      case CONCISE:
-        return formatConcise(player);
-      case FANCY:
-        return formatFancy(player);
-      case PLAIN:
-        return formatPlain(player);
-      case TAB:
-        return formatTab(player);
-      case VERBOSE:
-        return formatVerbose(player);
-      default:
-        return formatPlain(player);
-    }
-  }
-
   static Component of(UUID playerId, NameStyle style) {
     Player player = Bukkit.getPlayer(playerId);
     return player != null ? of(player, style) : UNKNOWN;
@@ -68,63 +34,105 @@ public interface PlayerComponent {
         : TranslatableComponent.of("misc.console", TextColor.DARK_AQUA);
   }
 
+  static Component of(Player player, NameStyle style) {
+    return of(player, "", style);
+  }
+
+  static Component of(@Nullable Player player, String defName, NameStyle style) {
+    // Offline player or not visible
+    if ((player == null || !player.isOnline())) {
+      return formatOffline(defName, style == NameStyle.PLAIN).build();
+    }
+
+    // For name styles that don't allow vanished, make vanished appear offline
+    if (!style.showVanish && isVanished(player)) {
+      return formatOffline(player.getName(), style == NameStyle.PLAIN).build();
+    }
+
+    TextComponent.Builder builder = TextComponent.builder();
+
+    switch (style) {
+      case COLOR:
+        builder = formatTeleport(formatColor(player), player.getName());
+        break;
+      case CONCISE:
+        builder = formatConcise(player);
+        break;
+      case FANCY:
+        builder = formatFancy(player);
+        break;
+      case TAB:
+        builder = formatTab(player);
+        break;
+      case VERBOSE:
+        builder = formatVerbose(player);
+        break;
+      default:
+        builder = formatPlain(player);
+        break;
+    }
+
+    return builder.build();
+  }
+
   // What an offline or vanished username renders as
-  static Component formatOffline(String name, boolean plain) {
-    TextComponent component = TextComponent.of(name);
-    if (!plain) component = component.color(TextColor.DARK_AQUA);
+  static TextComponent.Builder formatOffline(String name, boolean plain) {
+    TextComponent.Builder component = TextComponent.builder().append(name);
+    if (!plain) component.color(TextColor.DARK_AQUA);
     return component;
   }
 
   // No color or formatting, simply the name
-  static Component formatPlain(Player player) {
-    return TextComponent.of(player.getName());
+  static TextComponent.Builder formatPlain(Player player) {
+    return TextComponent.builder().append(player.getName());
   }
 
   // Color only
-  static Component formatColor(Player player) {
+  static TextComponent.Builder formatColor(Player player) {
     String displayName = player.getDisplayName();
     char colorChar = displayName.charAt((displayName.indexOf(player.getName()) - 1));
     TextColor color = TextFormatter.convert(ChatColor.getByChar(colorChar));
-    return TextComponent.of(player.getName(), color);
+    return TextComponent.builder().append(player.getName(), color);
   }
 
   // Color, flair & teleport
-  static Component formatFancy(Player player) {
-    Component prefix = getPrefixComponent(player);
-    Component colorName = formatColor(player);
+  static TextComponent.Builder formatFancy(Player player) {
+    TextComponent.Builder prefix = getPrefixComponent(player);
+    TextComponent.Builder colorName = formatColor(player);
+
     return formatTeleport(prefix.append(colorName), player.getName());
   }
 
   // Color, flair, death status, and vanish
-  static Component formatTab(Player player) {
-    Component prefix = getPrefixComponent(player);
-    Component colorName = formatColor(player);
+  static TextComponent.Builder formatTab(Player player) {
+    TextComponent.Builder prefix = getPrefixComponent(player);
+    TextComponent.Builder colorName = formatColor(player);
 
     if (isDead(player)) {
-      colorName = colorName.color(TextColor.DARK_GRAY);
+      colorName.color(TextColor.DARK_GRAY);
     }
 
     if (isVanished(player)) {
-      colorName = colorName.decoration(TextDecoration.STRIKETHROUGH, true);
+      colorName = formatVanished(colorName);
     }
 
     return prefix.append(colorName);
   }
 
   // Color, flair, and vanish status
-  static Component formatConcise(Player player) {
-    Component prefix = getPrefixComponent(player);
-    Component colorName = formatColor(player);
+  static TextComponent.Builder formatConcise(Player player) {
+    TextComponent.Builder prefix = getPrefixComponent(player);
+    TextComponent.Builder colorName = formatColor(player);
 
     if (isVanished(player)) {
-      colorName = colorName.decoration(TextDecoration.STRIKETHROUGH, true);
+      colorName = formatVanished(colorName);
     }
 
     return prefix.append(colorName);
   }
 
   // Color, flair, vanished, and teleport
-  static Component formatVerbose(Player player) {
+  static TextComponent.Builder formatVerbose(Player player) {
     return formatTeleport(formatConcise(player), player.getName());
   }
 
@@ -134,7 +142,7 @@ public interface PlayerComponent {
    * @param player The player
    * @return a component with a player's prefix
    */
-  static Component getPrefixComponent(Player player) {
+  static TextComponent.Builder getPrefixComponent(Player player) {
     String realName = player.getName();
     String displayName = player.getDisplayName();
     String prefix = displayName.substring(0, displayName.indexOf(realName) - 2);
@@ -157,17 +165,22 @@ public interface PlayerComponent {
       }
     }
 
-    return prefixComponent.build();
+    return prefixComponent;
   }
 
   // Format component to have teleport click/hover
-  static Component formatTeleport(Component username, String name) {
+  static TextComponent.Builder formatTeleport(TextComponent.Builder username, String name) {
     return username
         .hoverEvent(
             HoverEvent.of(
                 Action.SHOW_TEXT,
-                TranslatableComponent.of("misc.teleportTo", TextColor.GRAY, username)))
+                TranslatableComponent.of("misc.teleportTo", TextColor.GRAY, username.build())))
         .clickEvent(ClickEvent.runCommand("/tp " + name));
+  }
+
+  // Format for visible vanished players
+  static TextComponent.Builder formatVanished(TextComponent.Builder builder) {
+    return builder.decoration(TextDecoration.STRIKETHROUGH, true);
   }
 
   // Player state checks
