0ca682c2f2cc629d2149daf25515e42f97173c48
applenick <applenick@users.noreply.github.com>
2020-06-14 22:42:23 -0700

    Clean up NameStyle and PlayerComponent * Removed unused style attributes from NameStyle * Split up logic for PlayerComponent formatting
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- util/src/main/java/tc/oc/pgm/util/named/NameStyle.java
+++ util/src/main/java/tc/oc/pgm/util/named/NameStyle.java
@@ -5,43 +5,27 @@ package tc.oc.pgm.util.named;
  * only by context, and is independent of the viewer.
  */
 public enum NameStyle {
-  PLAIN(false, false, false, false, false, false, false, false, false), // No formatting
-  COLOR(true, false, false, false, false, false, false, true, false), // Color only
-  FANCY(
-      true, true, true, true, true, false, false, true,
-      false), // Color, flair, friend status, nick status, vanish status
-  TAB(
-      true, true, true, true, true, false, true, true,
-      true), // Color, flair, friend status, nick status, death status, and vanish status
-  VERBOSE(true, true, true, true, true, true, false, true, true), // Fancy plus nickname
-  CONCISE(true, true, true, true, true, true, false, false, true); // Verbose, but removes teleport
+  PLAIN(false, false, false, false, false), // No formatting
+  COLOR(true, false, false, true, false), // Color and teleport
+  FANCY(true, true, false, true, false), // Color, flair, and teleport
+  TAB(true, true, true, false, true), // Color, flair, death status, and vanish
+  VERBOSE(true, true, false, true, true), // Color, flair, teleport, and vanish
+  CONCISE(true, true, false, false, true); // Color, flair, and vanish
 
   public final boolean isColor;
   public final boolean showPrefix;
-  public final boolean showSelf; // Bold if self
-  public final boolean showFriend; // Italic if friend
-  public final boolean showDisguise; // Strikethrough if disguised
-  public final boolean showNickname; // Show nickname after real name
   public final boolean showDeath; // Grey out name if dead
   public final boolean teleport; // Click name to teleport
-  public final boolean showVanish; // Italic if vanished
+  public final boolean showVanish; // Whether to render name as online or not
 
   NameStyle(
       boolean isColor,
       boolean showPrefix,
-      boolean showSelf,
-      boolean showFriend,
-      boolean showDisguise,
-      boolean showNickname,
       boolean showDeath,
       boolean teleport,
       boolean showVanish) {
     this.isColor = isColor;
     this.showPrefix = showPrefix;
-    this.showSelf = showSelf;
-    this.showFriend = showFriend;
-    this.showDisguise = showDisguise;
-    this.showNickname = showNickname;
     this.showDeath = showDeath;
     this.teleport = teleport;
     this.showVanish = showVanish;
--- util/src/main/java/tc/oc/pgm/util/text/types/PlayerComponent.java
+++ util/src/main/java/tc/oc/pgm/util/text/types/PlayerComponent.java
@@ -28,63 +28,33 @@ public interface PlayerComponent {
   }
 
   static Component of(@Nullable Player player, String defName, NameStyle style) {
-    TextComponent.Builder component = TextComponent.builder();
 
-    // Offline player
-    if (player == null || !player.isOnline()) {
-      TextComponent name = TextComponent.of(defName);
-      if (style != NameStyle.PLAIN) name = name.color(TextColor.DARK_AQUA);
-      component.append(name);
-      return component.build();
+    // Offline player or not visible
+    if ((player == null || !player.isOnline())) {
+      return formatOffline(defName, style == NameStyle.PLAIN);
     }
 
     // For name styles that don't allow vanished, make vanished appear offline
     if (!style.showVanish && isVanished(player)) {
-      TextComponent name = TextComponent.of(player.getName());
-      if (style != NameStyle.PLAIN) name = name.color(TextColor.DARK_AQUA);
-      component.append(name);
-      return component.build();
+      return formatOffline(player.getName(), style == NameStyle.PLAIN);
     }
 
-    String realName = player.getName();
-    String displayName = player.getDisplayName();
-    String prefix = displayName.replace(realName, "");
-
-    char colorChar = displayName.charAt((displayName.indexOf(realName) - 1));
-    TextColor color =
-        style != NameStyle.PLAIN
-            ? TextFormatter.convert(ChatColor.getByChar(colorChar))
-            : TextColor.WHITE;
-
-    Component prefixComponent = TextComponent.of(prefix);
-    Component usernameComponent = TextComponent.of(realName, color);
-
-    if (!style.showPrefix) {
-      prefixComponent = TextComponent.empty();
+    switch (style) {
+      case COLOR:
+        return formatTeleport(formatColor(player), player.getName());
+      case CONCISE:
+        return formatConcise(player);
+      case FANCY:
+        return formatFancy(player);
+      case PLAIN:
+        return formatPlain(player);
+      case TAB:
+        return formatTab(player);
+      case VERBOSE:
+        return formatVerbose(player);
+      default:
+        return formatPlain(player);
     }
-
-    if (style.showDeath && isDead(player)) {
-      usernameComponent = usernameComponent.color(TextColor.DARK_GRAY);
-    }
-
-    if (style.showVanish && isVanished(player)) {
-      usernameComponent = usernameComponent.decoration(TextDecoration.STRIKETHROUGH, true);
-    }
-
-    Component formattedUsername =
-        TextComponent.builder().append(prefixComponent).append(usernameComponent).build();
-
-    component.append(formattedUsername);
-
-    if (style.teleport) {
-      component.hoverEvent(
-          HoverEvent.of(
-              Action.SHOW_TEXT,
-              TranslatableComponent.of("misc.teleportTo", TextColor.GRAY).args(formattedUsername)));
-      component.clickEvent(ClickEvent.runCommand("/tp " + player.getName()));
-    }
-
-    return component.build();
   }
 
   static Component of(UUID playerId, NameStyle style) {
@@ -98,6 +68,109 @@ public interface PlayerComponent {
         : TranslatableComponent.of("misc.console", TextColor.DARK_AQUA);
   }
 
+  // What an offline or vanished username renders as
+  static Component formatOffline(String name, boolean plain) {
+    TextComponent component = TextComponent.of(name);
+    if (!plain) component = component.color(TextColor.DARK_AQUA);
+    return component;
+  }
+
+  // No color or formatting, simply the name
+  static Component formatPlain(Player player) {
+    return TextComponent.of(player.getName());
+  }
+
+  // Color only
+  static Component formatColor(Player player) {
+    String displayName = player.getDisplayName();
+    char colorChar = displayName.charAt((displayName.indexOf(player.getName()) - 1));
+    TextColor color = TextFormatter.convert(ChatColor.getByChar(colorChar));
+    return TextComponent.of(player.getName(), color);
+  }
+
+  // Color, flair & teleport
+  static Component formatFancy(Player player) {
+    Component prefix = getPrefixComponent(player);
+    Component colorName = formatColor(player);
+    return formatTeleport(prefix.append(colorName), player.getName());
+  }
+
+  // Color, flair, death status, and vanish
+  static Component formatTab(Player player) {
+    Component prefix = getPrefixComponent(player);
+    Component colorName = formatColor(player);
+
+    if (isDead(player)) {
+      colorName = colorName.color(TextColor.DARK_GRAY);
+    }
+
+    if (isVanished(player)) {
+      colorName = colorName.decoration(TextDecoration.STRIKETHROUGH, true);
+    }
+
+    return prefix.append(colorName);
+  }
+
+  // Color, flair, and vanish status
+  static Component formatConcise(Player player) {
+    Component prefix = getPrefixComponent(player);
+    Component colorName = formatColor(player);
+
+    if (isVanished(player)) {
+      colorName = colorName.decoration(TextDecoration.STRIKETHROUGH, true);
+    }
+
+    return prefix.append(colorName);
+  }
+
+  // Color, flair, vanished, and teleport
+  static Component formatVerbose(Player player) {
+    return formatTeleport(formatConcise(player), player.getName());
+  }
+
+  /**
+   * Get the player's prefix as a {@link Component}
+   *
+   * @param player The player
+   * @return a component with a player's prefix
+   */
+  static Component getPrefixComponent(Player player) {
+    String realName = player.getName();
+    String displayName = player.getDisplayName();
+    String prefix = displayName.substring(0, displayName.indexOf(realName) - 2);
+
+    TextComponent.Builder prefixComponent = TextComponent.builder();
+    boolean isColor = false;
+    TextColor color = null;
+    for (int i = 0; i < prefix.length(); i++) {
+      if (prefix.charAt(i) == ChatColor.COLOR_CHAR) {
+        isColor = true;
+        continue;
+      }
+
+      if (isColor) {
+        color = TextFormatter.convert(ChatColor.getByChar(prefix.charAt(i)));
+        isColor = false;
+      } else {
+        prefixComponent.append(
+            String.valueOf(prefix.charAt(i)), color != null ? color : TextColor.WHITE);
+      }
+    }
+
+    return prefixComponent.build();
+  }
+
+  // Format component to have teleport click/hover
+  static Component formatTeleport(Component username, String name) {
+    return username
+        .hoverEvent(
+            HoverEvent.of(
+                Action.SHOW_TEXT,
+                TranslatableComponent.of("misc.teleportTo", TextColor.GRAY, username)))
+        .clickEvent(ClickEvent.runCommand("/tp " + name));
+  }
+
+  // Player state checks
   static boolean isVanished(Player player) {
     return player.hasMetadata("isVanished");
   }
