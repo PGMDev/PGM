d7b1e82b3cd8e4142f284815bf803408dcf63e91
KingSimon <19822231+KingOfSquares@users.noreply.github.com>
2020-12-03 22:55:39 +0100

    refactor
    Signed-off-by: KingSimon <19822231+KingOfSquares@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/filters/FilterParser.java
+++ core/src/main/java/tc/oc/pgm/filters/FilterParser.java
@@ -3,7 +3,9 @@ package tc.oc.pgm.filters;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.Nullable;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -457,9 +459,9 @@ public abstract class FilterParser {
     return new ScoreFilter(XMLUtils.parseNumericRange(new Node(el), Integer.class));
   }
 
-  @MethodParser("match-state")
-  public MatchStateFilter parseMatchState(Element el) throws InvalidXMLException {
-    return parseMatchStateFilter(el.getValue(), el);
+  @MethodParser("match-phase")
+  public Filter parseMatchPhase(Element el) throws InvalidXMLException {
+    return parseMatchPhaseFilter(el.getValue(), el);
   }
 
   @MethodParser("match-started")
