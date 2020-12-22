a4764d4ff6e05a3e0a1c586c50f163e006896d90
Pugzy <pugzy@mail.com>
2020-12-22 00:11:27 +0000

    Add query modifier for checking acting player info
    Signed-off-by: Pugzy <pugzy@mail.com>

--- core/src/main/java/tc/oc/pgm/filters/modifier/QueryModifier.java
+++ core/src/main/java/tc/oc/pgm/filters/modifier/QueryModifier.java
@@ -2,6 +2,7 @@ package tc.oc.pgm.filters.modifier;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import javax.annotation.Nullable;
 import tc.oc.pgm.api.filter.Filter;
 import tc.oc.pgm.api.filter.query.Query;
 import tc.oc.pgm.filters.TypedFilter;
@@ -9,6 +10,9 @@ import tc.oc.pgm.filters.TypedFilter;
 /**
  * Takes a query and modifies it before passing it onto its child filter. Might transform query type
  * so {@link #getQueryType()} on this modifier can be different from its child filter
+ *
+ * <p>If the modified query returns {@code null} this modifier will abstain without querying the
+ * child filter.
  *
  * @param <Q> is the type of query this filter can modify before passing it on to its child filter
  */
@@ -21,11 +25,14 @@ public abstract class QueryModifier<Q extends Query> extends TypedFilter<Q> {
   }
 
   /** Returns a modified {@link Query} */
+  @Nullable
   protected abstract Query modifyQuery(Q query);
 
   public abstract Class<? extends Q> getQueryType();
 
   protected QueryResponse queryTyped(Q query) {
-    return filter.query(modifyQuery(query));
+    Query modifiedQuery = modifyQuery(query);
+
+    return modifiedQuery == null ? QueryResponse.ABSTAIN : filter.query(modifiedQuery);
   }
 }
