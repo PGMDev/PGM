58873e771f7b64556a3a0a47065d119502149162
Pablete1234 <pabloherrerapalacio@gmail.com>
2020-12-27 13:10:49 +0100

    Implement filters on respawns & allow multiple of them
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/spawns/SpawnModule.java
+++ core/src/main/java/tc/oc/pgm/spawns/SpawnModule.java
@@ -4,16 +4,20 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import java.time.Duration;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 import net.kyori.adventure.text.Component;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import tc.oc.pgm.api.filter.Filter;
 import tc.oc.pgm.api.map.MapModule;
 import tc.oc.pgm.api.map.factory.MapFactory;
 import tc.oc.pgm.api.map.factory.MapModuleFactory;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.match.MatchModule;
+import tc.oc.pgm.filters.FilterParser;
+import tc.oc.pgm.filters.StaticFilter;
 import tc.oc.pgm.kits.KitModule;
 import tc.oc.pgm.points.PointParser;
 import tc.oc.pgm.regions.RegionModule;
@@ -24,14 +28,15 @@ import tc.oc.pgm.util.xml.XMLUtils;
 
 public class SpawnModule implements MapModule {
 
+  public static final Duration DEFAULT_RESPAWN_DELAY = Duration.ofMillis(2000);
   public static final Duration MINIMUM_RESPAWN_DELAY = Duration.ofMillis(1500);
   public static final Duration IGNORE_CLICKS_DELAY = Duration.ofMillis(500);
 
   protected final Spawn defaultSpawn;
   protected final List<Spawn> spawns;
-  protected final RespawnOptions respawnOptions;
+  protected final List<RespawnOptions> respawnOptions;
 
-  public SpawnModule(Spawn defaultSpawn, List<Spawn> spawns, RespawnOptions respawnOptions) {
+  public SpawnModule(Spawn defaultSpawn, List<Spawn> spawns, List<RespawnOptions> respawnOptions) {
     assert defaultSpawn != null;
     this.defaultSpawn = defaultSpawn;
     this.spawns = spawns;
@@ -44,6 +49,9 @@ public class SpawnModule implements MapModule {
   }
 
   public static class Factory implements MapModuleFactory<SpawnModule> {
+
+    private FilterParser filterParser;
+
     @Override
     public Collection<Class<? extends MapModule>> getSoftDependencies() {
       return ImmutableList.of(RegionModule.class, KitModule.class);
@@ -57,6 +65,7 @@ public class SpawnModule implements MapModule {
     @Override
     public SpawnModule parse(MapFactory factory, Logger logger, Document doc)
         throws InvalidXMLException {
+      this.filterParser = factory.getFilters();
       SpawnParser parser = new SpawnParser(factory, new PointParser(factory));
       List<Spawn> spawns = Lists.newArrayList();
 
@@ -70,27 +79,56 @@ public class SpawnModule implements MapModule {
 
       return new SpawnModule(parser.getDefaultSpawn(), spawns, parseRespawnOptions(doc));
     }
-  }
 
-  protected static RespawnOptions parseRespawnOptions(Document doc) throws InvalidXMLException {
-    Duration delay = MINIMUM_RESPAWN_DELAY;
-    boolean auto = doc.getRootElement().getChild("autorespawn") != null; // Legacy support
-    boolean blackout = false;
-    boolean spectate = false;
-    boolean bedSpawn = false;
-    Component message = null;
+    private List<RespawnOptions> parseRespawnOptions(Document doc) throws InvalidXMLException {
+      List<RespawnOptions> respawnOptions = Lists.newArrayList();
 
-    for (Element elRespawn : doc.getRootElement().getChildren("respawn")) {
-      delay = XMLUtils.parseDuration(elRespawn.getAttribute("delay"), delay);
-      auto = XMLUtils.parseBoolean(elRespawn.getAttribute("auto"), auto);
-      blackout = XMLUtils.parseBoolean(elRespawn.getAttribute("blackout"), blackout);
-      spectate = XMLUtils.parseBoolean(elRespawn.getAttribute("spectate"), spectate);
-      bedSpawn = XMLUtils.parseBoolean(elRespawn.getAttribute("bed"), bedSpawn);
-      message = XMLUtils.parseFormattedText(elRespawn, "message", message);
+      for (Element elRespawn :
+          XMLUtils.flattenElements(doc.getRootElement(), "respawns", "respawn")) {
+        respawnOptions.add(getRespawnOptions(elRespawn));
+      }
+      // Parse root children respawn elements, Keeps old syntax and gives a default spawn if all
+      // others fail
+      respawnOptions.add(
+          getRespawnOptions(
+              doc.getRootElement().getChildren("respawn"),
+              doc.getRootElement().getChild("autorespawn") != null,
+              true));
 
-      if (TimeUtils.isShorterThan(delay, MINIMUM_RESPAWN_DELAY)) delay = MINIMUM_RESPAWN_DELAY;
+      return respawnOptions;
     }
 
-    return new RespawnOptions(delay, auto, blackout, spectate, bedSpawn, message);
+    private RespawnOptions getRespawnOptions(Element element) throws InvalidXMLException {
+      return getRespawnOptions(Collections.singleton(element), false, false);
+    }
+
+    protected RespawnOptions getRespawnOptions(
+        Collection<Element> elements, boolean autorespawn, boolean topLevel)
+        throws InvalidXMLException {
+      Duration delay = DEFAULT_RESPAWN_DELAY;
+      boolean auto = autorespawn;
+      boolean blackout = false;
+      boolean spectate = false;
+      boolean bedSpawn = false;
+      Filter filter = StaticFilter.ALLOW;
+      Component message = null;
+
+      for (Element elRespawn : elements) {
+        delay = XMLUtils.parseDuration(elRespawn.getAttribute("delay"), delay);
+        auto = XMLUtils.parseBoolean(elRespawn.getAttribute("auto"), auto);
+        blackout = XMLUtils.parseBoolean(elRespawn.getAttribute("blackout"), blackout);
+        spectate = XMLUtils.parseBoolean(elRespawn.getAttribute("spectate"), spectate);
+        bedSpawn = XMLUtils.parseBoolean(elRespawn.getAttribute("bed"), bedSpawn);
+        filter = filterParser.parseFilterProperty(elRespawn, "filter", filter);
+        if (filter != StaticFilter.ALLOW && topLevel)
+          throw new InvalidXMLException("Parent respawn elements can't use filters", elRespawn);
+
+        message = XMLUtils.parseFormattedText(elRespawn, "message", message);
+
+        if (TimeUtils.isShorterThan(delay, MINIMUM_RESPAWN_DELAY)) delay = MINIMUM_RESPAWN_DELAY;
+      }
+
+      return new RespawnOptions(delay, auto, blackout, spectate, bedSpawn, filter, message);
+    }
   }
 }
