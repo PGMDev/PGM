c33780ef342f988017adfe7d0d139a375437d89f
KingSimon <19822231+KingOfSquares@users.noreply.github.com>
2021-01-14 22:17:36 +0100

    Remove duplicate code
    Signed-off-by: KingSimon <19822231+KingOfSquares@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/observers/ObserverToolsMatchModule.java
+++ core/src/main/java/tc/oc/pgm/observers/ObserverToolsMatchModule.java
@@ -3,20 +3,15 @@ package tc.oc.pgm.observers;
 import static net.kyori.adventure.text.Component.translatable;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import java.util.List;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.match.MatchModule;
 import tc.oc.pgm.api.match.MatchScope;
@@ -32,7 +27,6 @@ import tc.oc.pgm.spawns.events.ObserverKitApplyEvent;
 import tc.oc.pgm.util.menu.InventoryMenu;
 import tc.oc.pgm.util.menu.InventoryMenuItem;
 import tc.oc.pgm.util.menu.pattern.SingleRowMenuArranger;
-import tc.oc.pgm.util.text.TextTranslations;
 
 @ListenerScope(MatchScope.LOADED)
 public class ObserverToolsMatchModule implements MatchModule, Listener {
@@ -100,24 +94,11 @@ public class ObserverToolsMatchModule implements MatchModule, Listener {
 
   private void refreshKit(MatchPlayer player) {
     if (canUse(player)) {
-      player.getInventory().setItem(TOOL_BUTTON_SLOT, createToolItem(player));
+      player.getInventory().setItem(TOOL_BUTTON_SLOT, toolItem.createItem(player.getBukkit()));
     }
   }
 
   private boolean isRightClick(Action action) {
     return action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK;
-  }
-
-  private ItemStack createToolItem(MatchPlayer player) {
-    ItemStack tool = new ItemStack(TOOL_MATERIAL);
-    ItemMeta meta = tool.getItemMeta();
-    Component displayName =
-        translatable("setting.displayName", NamedTextColor.AQUA, TextDecoration.BOLD);
-    Component lore = translatable("setting.lore", NamedTextColor.GRAY);
-    meta.setDisplayName(TextTranslations.translateLegacy(displayName, player.getBukkit()));
-    meta.setLore(Lists.newArrayList(TextTranslations.translateLegacy(lore, player.getBukkit())));
-    meta.addItemFlags(ItemFlag.values());
-    tool.setItemMeta(meta);
-    return tool;
   }
 }
