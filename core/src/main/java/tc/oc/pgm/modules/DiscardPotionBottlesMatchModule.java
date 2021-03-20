014bb01c07324b168fa57cdddd4bfc54f9bb9203
cswhite2000 <18whitechristop@gmail.com>
2021-03-19 19:25:18 -0700

    Fix DiscardPotionBottlesMatchModule for Spigot
    Signed-off-by: cswhite2000 <18whitechristop@gmail.com>

--- core/src/main/java/tc/oc/pgm/modules/DiscardPotionBottlesMatchModule.java
+++ core/src/main/java/tc/oc/pgm/modules/DiscardPotionBottlesMatchModule.java
@@ -1,6 +1,8 @@
 package tc.oc.pgm.modules;
 
+import java.util.concurrent.TimeUnit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,13 +17,34 @@ import tc.oc.pgm.util.bukkit.BukkitUtils;
 @ListenerScope(MatchScope.RUNNING)
 public class DiscardPotionBottlesMatchModule implements MatchModule, Listener {
 
-  public DiscardPotionBottlesMatchModule(Match match) {}
+  Match match;
+
+  public DiscardPotionBottlesMatchModule(Match match) {
+    this.match = match;
+  }
 
   @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
   public void onDrinkPotion(final PlayerItemConsumeEvent event) {
-    if (!BukkitUtils.isSportPaper()) return; // TODO: implement this
     if (event.getItem().getType() == Material.POTION) {
-      event.setReplacement(new ItemStack(Material.AIR));
+      if (BukkitUtils.isSportPaper()) {
+        event.setReplacement(new ItemStack(Material.AIR));
+      } else {
+        int itemSlot = event.getPlayer().getInventory().getHeldItemSlot();
+        Player player = event.getPlayer();
+
+        // Setting the event item should work, but Spigot doesn't have proper error checking for
+        // this so if you set the event item to air it throws a null pointer
+        match
+            .getExecutor(MatchScope.LOADED)
+            .schedule(
+                () -> {
+                  if (player.getInventory().getItem(itemSlot).getType() == Material.GLASS_BOTTLE) {
+                    player.getInventory().setItem(itemSlot, new ItemStack(Material.AIR));
+                  }
+                },
+                0,
+                TimeUnit.MILLISECONDS);
+      }
     }
   }
 }
