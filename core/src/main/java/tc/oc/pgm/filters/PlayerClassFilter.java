b4c4e575a5bce52ebcf047b5a16da376c544f916
Pugzy <pugzy@mail.com>
2021-08-23 00:29:38 +0100

    Remove class as a dynamic filter
    Signed-off-by: KingSimon <19822231+KingOfSquares@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/filters/PlayerClassFilter.java
+++ core/src/main/java/tc/oc/pgm/filters/PlayerClassFilter.java
@@ -1,24 +1,15 @@
 package tc.oc.pgm.filters;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
-import java.util.Collection;
-import org.bukkit.event.Event;
 import tc.oc.pgm.api.filter.query.PlayerQuery;
 import tc.oc.pgm.classes.ClassMatchModule;
 import tc.oc.pgm.classes.PlayerClass;
-import tc.oc.pgm.classes.PlayerClassChangeEvent;
 
 public class PlayerClassFilter extends TypedFilter<PlayerQuery> {
   protected final PlayerClass playerClass;
 
   public PlayerClassFilter(PlayerClass playerClass) {
     this.playerClass = Preconditions.checkNotNull(playerClass, "player class");
-  }
-
-  @Override
-  public Collection<Class<? extends Event>> getRelevantEvents() {
-    return ImmutableList.of(PlayerClassChangeEvent.class);
   }
 
   @Override
