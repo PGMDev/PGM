ddd00fdb57e46c5d997a291469a7276288ca3c6f
Pablete1234 <pabloherrerapalacio@gmail.com>
2022-04-24 13:32:34 +0200

    Implement players filter
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/api/filter/Filter.java
+++ core/src/main/java/tc/oc/pgm/api/filter/Filter.java
@@ -32,6 +32,10 @@ public interface Filter {
     return ImmutableList.of();
   }
 
+  default boolean isDynamic() {
+    return !getRelevantEvents().isEmpty();
+  }
+
   /** Least-derived query type that this filter might not abstain from */
   Class<? extends Query> getQueryType();
 
