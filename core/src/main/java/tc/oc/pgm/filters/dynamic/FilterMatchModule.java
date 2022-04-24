d8283d865d5004fefe67003a40cff4bc013b1e8c
Pablete1234 <pabloherrerapalacio@gmail.com>
2022-04-24 13:32:34 +0200

    Implement players filter
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/filters/dynamic/FilterMatchModule.java
+++ core/src/main/java/tc/oc/pgm/filters/dynamic/FilterMatchModule.java
@@ -97,7 +97,7 @@ public class FilterMatchModule implements MatchModule, FilterDispatcher, Tickabl
         .rowKeySet()
         .forEach(
             filter -> {
-              if (filter.getRelevantEvents().isEmpty()) {
+              if (!filter.isDynamic()) {
                 match
                     .getLogger()
                     .warning("Filter " + filter + " was submitted as a dynamic filter but is not!");
