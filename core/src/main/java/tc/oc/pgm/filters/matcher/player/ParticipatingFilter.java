8b90a607d2cb809b7a2e5c8c6f0eab9d9ea3ef22
Pablete1234 <pabloherrerapalacio@gmail.com>
2022-07-26 10:01:09 +0200

    Make participating dynamic
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/filters/matcher/player/ParticipatingFilter.java
+++ core/src/main/java/tc/oc/pgm/filters/matcher/player/ParticipatingFilter.java
@@ -1,7 +1,12 @@
 package tc.oc.pgm.filters.matcher.player;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.bukkit.event.Event;
 import tc.oc.pgm.api.filter.query.PartyQuery;
+import tc.oc.pgm.api.match.event.MatchPhaseChangeEvent;
 import tc.oc.pgm.api.party.Party;
+import tc.oc.pgm.events.PlayerPartyChangeEvent;
 import tc.oc.pgm.filters.matcher.TypedFilter;
 
 public class ParticipatingFilter extends TypedFilter.Impl<PartyQuery> {
@@ -13,6 +18,11 @@ public class ParticipatingFilter extends TypedFilter.Impl<PartyQuery> {
 
   public ParticipatingFilter(boolean participating) {
     this.participating = participating;
+  }
+
+  @Override
+  public Collection<Class<? extends Event>> getRelevantEvents() {
+    return ImmutableList.of(MatchPhaseChangeEvent.class, PlayerPartyChangeEvent.class);
   }
 
   @Override
