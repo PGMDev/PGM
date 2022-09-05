9dc7d61a98e9983e0111b827f93459e09905845d
Pablete1234 <pabloherrerapalacio@gmail.com>
2022-09-05 21:17:06 +0200

    Fix variable filter not allowing parent scopes
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/filters/matcher/match/VariableFilter.java
+++ core/src/main/java/tc/oc/pgm/filters/matcher/match/VariableFilter.java
@@ -1,11 +1,14 @@
 package tc.oc.pgm.filters.matcher.match;
 
 import com.google.common.collect.Range;
+import tc.oc.pgm.api.filter.Filterables;
+import tc.oc.pgm.api.filter.query.MatchQuery;
+import tc.oc.pgm.api.filter.query.Query;
 import tc.oc.pgm.filters.Filterable;
-import tc.oc.pgm.filters.matcher.TypedFilter;
+import tc.oc.pgm.filters.matcher.WeakTypedFilter;
 import tc.oc.pgm.variables.VariableDefinition;
 
-public class VariableFilter<T extends Filterable<?>> implements TypedFilter<T> {
+public class VariableFilter<T extends Filterable<?>> implements WeakTypedFilter<MatchQuery> {
 
   private final VariableDefinition<T> variable;
   private final Range<Double> values;
@@ -22,12 +25,28 @@ public class VariableFilter<T extends Filterable<?>> implements TypedFilter<T> {
   }
 
   @Override
-  public boolean matches(T query) {
-    return values.contains(variable.getVariable(query.getMatch()).getValue(query));
+  public Class<MatchQuery> queryType() {
+    return MatchQuery.class;
   }
 
   @Override
-  public Class<? extends T> queryType() {
-    return variable.getScope();
+  public QueryResponse queryTyped(MatchQuery query) {
+    Filterable<?> filterable = query.extractFilterable();
+    if (!respondsTo(filterable.getClass())) return QueryResponse.ABSTAIN;
+
+    return QueryResponse.fromBoolean(
+        values.contains(variable.getVariable(query.getMatch()).getValue(filterable)));
+  }
+
+  @Override
+  public boolean respondsTo(Class<? extends Query> queryType) {
+    //noinspection unchecked
+    return Filterable.class.isAssignableFrom(queryType)
+        && Filterables.isAssignable((Class<Filterable<?>>) queryType, variable.getScope());
+  }
+
+  @Override
+  public String toString() {
+    return "VariableFilter{" + "variable=" + variable + ", values=" + values + '}';
   }
 }
