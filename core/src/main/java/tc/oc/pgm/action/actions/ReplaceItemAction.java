b6d8732eaca6565fb5b6f4f122c449b98a186f2f
applenick <applenick@users.noreply.github.com>
2022-09-28 11:33:44 -0700

    Check armor slots when replacing item
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/action/actions/ReplaceItemAction.java
+++ core/src/main/java/tc/oc/pgm/action/actions/ReplaceItemAction.java
@@ -1,7 +1,7 @@
 package tc.oc.pgm.action.actions;
 
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.util.inventory.ItemMatcher;
 
@@ -23,16 +23,29 @@ public class ReplaceItemAction extends AbstractAction<MatchPlayer> {
 
   @Override
   public void trigger(MatchPlayer matchPlayer) {
-    Inventory inv = matchPlayer.getInventory();
+    PlayerInventory inv = matchPlayer.getInventory();
+
+    ItemStack[] armor = inv.getArmorContents();
+    for (int i = 0; i < armor.length; i++) {
+      ItemStack current = armor[i];
+      if (current == null || !matcher.matches(current)) continue;
+      armor[i] = replaceItem(current);
+    }
+    inv.setArmorContents(armor);
+
     for (int i = 0; i < inv.getSize(); i++) {
       ItemStack current = inv.getItem(i);
       if (current == null || !matcher.matches(current)) continue;
 
-      ItemStack newItem = item.clone();
-      if (keepAmount) newItem.setAmount(current.getAmount());
-      if (keepEnchants) newItem.addEnchantments(current.getEnchantments());
-
+      ItemStack newItem = replaceItem(current);
       inv.setItem(i, newItem);
     }
+  }
+
+  private ItemStack replaceItem(ItemStack current) {
+    ItemStack newItem = item.clone();
+    if (keepAmount) newItem.setAmount(current.getAmount());
+    if (keepEnchants) newItem.addEnchantments(current.getEnchantments());
+    return newItem;
   }
 }
