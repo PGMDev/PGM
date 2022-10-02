0ad7b60bc61c1a2a7793c7c5f491b18eb1a2c95b
Pablete1234 <pabloherrerapalacio@gmail.com>
2022-10-02 22:33:55 +0200

    Fix missing events & simplify logic
    Signed-off-by: Pablete1234 <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/filters/matcher/player/PlayerStateFilter.java
+++ core/src/main/java/tc/oc/pgm/filters/matcher/player/PlayerStateFilter.java
@@ -4,9 +4,11 @@ import com.google.common.collect.ImmutableList;
 import java.util.Collection;
 import org.bukkit.event.Event;
 import tc.oc.pgm.api.filter.query.PlayerQuery;
+import tc.oc.pgm.api.match.event.MatchPhaseChangeEvent;
 import tc.oc.pgm.api.player.MatchPlayer;
-import tc.oc.pgm.api.player.event.MatchPlayerDeathEvent;
-import tc.oc.pgm.spawns.events.PlayerSpawnEvent;
+import tc.oc.pgm.events.PlayerPartyChangeEvent;
+import tc.oc.pgm.spawns.events.ParticipantDespawnEvent;
+import tc.oc.pgm.spawns.events.ParticipantSpawnEvent;
 
 public class PlayerStateFilter extends ParticipantFilter {
 
@@ -21,11 +23,15 @@ public class PlayerStateFilter extends ParticipantFilter {
 
   @Override
   public Collection<Class<? extends Event>> getRelevantEvents() {
-    return ImmutableList.of(PlayerSpawnEvent.class, MatchPlayerDeathEvent.class);
+    return ImmutableList.of(
+        ParticipantSpawnEvent.class,
+        ParticipantDespawnEvent.class,
+        MatchPhaseChangeEvent.class,
+        PlayerPartyChangeEvent.class);
   }
 
   @Override
   protected boolean matches(PlayerQuery query, MatchPlayer player) {
-    return this.alive ? player.isAlive() : player.isDead();
+    return player.isParticipating() && this.alive != player.isDead();
   }
 }
