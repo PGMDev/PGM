29d6cca3f0b7be489f2572292a8587ff8ee0431f
Patrick <cowinkkeydinkinc@gmail.com>
2022-10-16 19:06:34 -0600

    Use both tags to display Blitz: Rage rather than new tags
    Signed-off-by: Patrick <cowinkkeydinkinc@gmail.com>

--- core/src/main/java/tc/oc/pgm/scoreboard/SidebarMatchModule.java
+++ core/src/main/java/tc/oc/pgm/scoreboard/SidebarMatchModule.java
@@ -300,10 +300,33 @@ public class SidebarMatchModule implements MatchModule, Listener {
       break;
     }
 
+    boolean isBlitz = false;
+    boolean isRage = false;
+    for (final MapTag tag : map.getTags()) {
+      if (tag.getId().matches("blitz")) {
+        isBlitz = true;
+        continue;
+      }
+      if (tag.getId().matches("rage")) {
+        isRage = true;
+      }
+    }
     // Second, append auxiliary game modes
     for (final MapTag tag : map.getTags()) {
       if (!tag.isGamemode() || !tag.isAuxiliary()) continue;
 
+      // Display "Blitz: Rage" instead of "Blitz and Rage"
+      if (isBlitz && isRage && games.size() < 2) {
+        if (tag.getId().matches("blitz")) {
+          // Add this only once
+          games.add(translatable("gamemode.br.name").color(NamedTextColor.AQUA));
+          continue;
+        }
+        if (tag.getId().matches("rage")) {
+          // Do not display "Rage"
+          continue;
+        }
+      }
       // There can only be 2 game modes
       if (games.size() < 2) {
         games.add(tag.getName().color(NamedTextColor.AQUA));
