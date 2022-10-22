8e8b1256f700bb7e5bf67c1f71e430528eabc0e6
Patrick <cowinkkeydinkinc@gmail.com>
2022-10-22 01:50:50 -0600

    Import class and add missing bracket
    Signed-off-by: Patrick <cowinkkeydinkinc@gmail.com>

--- core/src/main/java/tc/oc/pgm/scoreboard/SidebarMatchModule.java
+++ core/src/main/java/tc/oc/pgm/scoreboard/SidebarMatchModule.java
@@ -22,6 +22,7 @@ import java.util.UUID;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -313,8 +314,9 @@ public class SidebarMatchModule implements MatchModule, Listener {
     }
 
     // Display "Blitz: Rage" rather than "Blitz and Rage"
-    if (games.size() == 2 && Stream.of("blitz", "rage").allMatch(
-            id-> map.getTags().stream().anyMatch(mt -> mt.getId().equals(id))) {
+    if (games.size() == 2
+        && Stream.of("blitz", "rage")
+            .allMatch(id -> map.getTags().stream().anyMatch(mt -> mt.getId().equals(id)))) {
       games.clear();
       games.add(translatable("gamemode.br.name").color(NamedTextColor.AQUA));
     }
