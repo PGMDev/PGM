a5b0f500ae314cbf2e1752f2559d1cd0b1c3e5da
OhPointFive <ohpointfive@gmail.com>
2024-07-07 20:29:07 -0400

    Sort gamemodes alphabetically by enum constant
    Signed-off-by: OhPointFive <ohpointfive@gmail.com>

--- core/src/main/java/tc/oc/pgm/api/map/Gamemode.java
+++ core/src/main/java/tc/oc/pgm/api/map/Gamemode.java
@@ -1,31 +1,31 @@
 package tc.oc.pgm.api.map;
 
 public enum Gamemode {
-  FIVE_CONTROL_POINT("5cp", "5 Control Points", "5CP"),
-  ATTACK_DEFEND("ad", "Attack/Defend", "A/D"),
   ARCADE("arcade", "Arcade", "Arcade"),
+  ATTACK_DEFEND("ad", "Attack/Defend", "A/D"),
   BEDWARS("bedwars", "Bed Wars", "Bed Wars"),
   BLITZ("blitz", "Blitz", "Blitz"),
   BLITZ_RAGE("br", "Blitz: Rage", "Blitz: Rage"),
   BRIDGE("bridge", "Bridge", "Bridge"),
   CAPTURE_THE_FLAG("ctf", "Capture the Flag", "CTF"),
-  CONTROL_THE_POINT("cp", "Control the Point", "CP"),
   CAPTURE_THE_WOOL("ctw", "Capture the Wool", "CTW"),
+  CONTROL_THE_POINT("cp", "Control the Point", "CP"),
+  DEATHMATCH("tdm", "Deathmatch", "TDM"),
   DESTROY_THE_CORE("dtc", "Destroy the Core", "DTC"),
   DESTROY_THE_MONUMENT("dtm", "Destroy the Monument", "DTM"),
-  FREE_FOR_ALL("ffa", "Free for All", "FFA"),
+  FIVE_CONTROL_POINT("5cp", "5 Control Points", "5CP"),
   FLAG_FOOTBALL("ffb", "Flag Football", "FFB"),
+  FREE_FOR_ALL("ffa", "Free for All", "FFA"),
   INFECTION("infection", "Infection", "Infection"),
-  KING_OF_THE_HILL("koth", "King of the Hill", "KotH"),
   KING_OF_THE_FLAG("kotf", "King of the Flag", "KotF"),
+  KING_OF_THE_HILL("koth", "King of the Hill", "KotH"),
   MIXED("mixed", "Mixed", "Mixed"),
   PAYLOAD("payload", "Payload", "Payload"),
-  RAGE("rage", "Rage", "Rage"),
   RACE_FOR_WOOL("rfw", "Race for Wool", "RFW"),
+  RAGE("rage", "Rage", "Rage"),
   SCOREBOX("scorebox", "Scorebox", "Scorebox"),
   SKYWARS("skywars", "Skywars", "Skywars"),
-  SURVIVAL_GAMES("sg", "Survival Games", "SG"),
-  DEATHMATCH("tdm", "Deathmatch", "TDM");
+  SURVIVAL_GAMES("sg", "Survival Games", "SG");
 
   private final String id;
 
