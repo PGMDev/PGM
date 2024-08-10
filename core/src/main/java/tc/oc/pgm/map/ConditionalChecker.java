b55573e3fff6ff3361e6691d9a13e8e390133660
Pablo Herrera <pabloherrerapalacio@gmail.com>
2024-08-10 03:24:06 +0200

    Cleanup conditionals further & add version checks
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/map/ConditionalChecker.java
+++ core/src/main/java/tc/oc/pgm/map/ConditionalChecker.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.jdom2.Element;
+import tc.oc.pgm.util.platform.Platform;
 import tc.oc.pgm.util.xml.InvalidXMLException;
 import tc.oc.pgm.util.xml.Node;
 import tc.oc.pgm.util.xml.XMLUtils;
@@ -15,7 +16,9 @@ class ConditionalChecker {
   private static final List<AttributeCheck> ATTRIBUTES = List.of(
       new AttributeCheck("variant", ConditionalChecker::variant),
       new AttributeCheck("has-variant", ConditionalChecker::hasVariant),
-      new AttributeCheck("constant", ConditionalChecker::constant));
+      new AttributeCheck("constant", ConditionalChecker::constant),
+      new AttributeCheck("min-server-version", ConditionalChecker::minServerVersion),
+      new AttributeCheck("max-server-version", ConditionalChecker::maxServerVersion));
   private static final String ALL_ATTRS =
       ATTRIBUTES.stream().map(AttributeCheck::key).collect(Collectors.joining("', '", "'", "'"));
 
@@ -24,7 +27,7 @@ class ConditionalChecker {
    *
    * @param ctx the map's context
    * @param el The conditional element
-   * @return true if the conditional
+   * @return if the conditional passes
    * @throws InvalidXMLException if the element is invalid in any way
    */
   static boolean test(MapFilePreprocessor ctx, Element el) throws InvalidXMLException {
@@ -34,28 +37,39 @@ class ConditionalChecker {
       if (attRes != null) result = result == null ? attRes : result && attRes;
     }
 
-    if (result == null)
-      throw new InvalidXMLException("Expected at least one of " + ALL_ATTRS + " attributes", el);
-
-    return result;
+    if (result != null) return result;
+    throw new InvalidXMLException("Expected at least one of " + ALL_ATTRS + " attributes", el);
   }
 
   private static String[] split(String val) {
     return val.split("[\\s,]+");
   }
 
-  private static boolean variant(MapFilePreprocessor ctx, Element el, String value) {
+  private static boolean variant(MapFilePreprocessor ctx, Element el, Node node) {
+    String value = node.getValue();
     if (value.indexOf(',') == -1) return value.equals(ctx.getVariant());
     return Set.of(split(value)).contains(ctx.getVariant());
   }
 
-  private static boolean hasVariant(MapFilePreprocessor ctx, Element el, String value) {
+  private static boolean hasVariant(MapFilePreprocessor ctx, Element el, Node node) {
+    String value = node.getValue();
     if (value.indexOf(',') == -1) return ctx.getVariantIds().contains(value);
     return Arrays.stream(split(value)).anyMatch(ctx.getVariantIds()::contains);
   }
 
-  private static boolean constant(MapFilePreprocessor ctx, Element el, String id)
+  private static boolean minServerVersion(MapFilePreprocessor ctx, Element el, Node node)
       throws InvalidXMLException {
+    return Platform.MINECRAFT_VERSION.isNoOlderThan(XMLUtils.parseSemanticVersion(node));
+  }
+
+  private static boolean maxServerVersion(MapFilePreprocessor ctx, Element el, Node node)
+      throws InvalidXMLException {
+    return Platform.MINECRAFT_VERSION.isNoNewerThan(XMLUtils.parseSemanticVersion(node));
+  }
+
+  private static boolean constant(MapFilePreprocessor ctx, Element el, Node node)
+      throws InvalidXMLException {
+    var id = node.getValue();
     var value = Node.fromAttr(el, "constant-value");
     var cmp = XMLUtils.parseEnum(
         Node.fromAttr(el, "constant-comparison"),
@@ -63,40 +77,40 @@ class ConditionalChecker {
         value == null ? Cmp.DEFINED : Cmp.EQUALS);
 
     var constants = ctx.getConstants();
+    var isDefined = constants.containsKey(id);
+    var constant = isDefined ? constants.get(id) : null;
 
-    if (!cmp.requireValue) {
-      if (value != null)
-        throw new InvalidXMLException("Comparison type " + cmp + " should not have a value", value);
+    if (!cmp.requireValue && value != null)
+      throw new InvalidXMLException("Comparison type " + cmp + " should not have a value", value);
 
-      if (!constants.containsKey(id)) return cmp == Cmp.UNDEFINED;
-
-      return switch (cmp) {
-        case DEFINED -> true;
-        case DEFINED_DELETE -> constants.get(id) == null;
-        case DEFINED_VALUE -> constants.get(id) != null;
-          // Should never happen
-        default -> throw new IllegalStateException(cmp + " not supported");
-      };
-    } else {
-      String constant = constants.get(id);
-      if (constant == null) {
-        if (!constants.containsKey(id))
-          throw new InvalidXMLException(
-              "Unknown constant '" + id + "'. Only constants before the conditional may be used.",
-              el);
-        return false;
-      }
+    if (cmp.requireValue) {
       if (value == null)
         throw new InvalidXMLException("Required attribute 'constant-value' not set", el);
 
+      if (!isDefined)
+        throw new InvalidXMLException(
+            "Unknown constant '" + id + "'. Only constants before the conditional may be used.",
+            el);
+      if (constant == null) return false;
+    }
+
+    // The only reason these are split is for the IDE to infer nullability
+    if (!cmp.requireValue) {
+      return switch (cmp) {
+        case UNDEFINED -> !isDefined;
+        case DEFINED -> isDefined;
+        case DEFINED_DELETE -> isDefined && constant == null;
+        case DEFINED_VALUE -> isDefined && constant != null;
+        default -> throw new IllegalStateException("Unexpected value: " + cmp);
+      };
+    } else {
       return switch (cmp) {
         case EQUALS -> Objects.equals(value.getValue(), constant);
         case CONTAINS -> Set.of(split(value.getValue())).contains(constant);
         case REGEX -> constant.matches(value.getValue());
         case RANGE -> XMLUtils.parseNumericRange(value, Double.class)
             .contains(XMLUtils.parseNumber(new Node(el), constant, Double.class, true));
-          // Should never happen
-        default -> throw new IllegalStateException(cmp + " not supported");
+        default -> throw new IllegalStateException("Unexpected value: " + cmp);
       };
     }
   }
@@ -118,15 +132,15 @@ class ConditionalChecker {
   }
 
   interface ElementPredicate {
-    boolean test(MapFilePreprocessor ctx, Element el, String value) throws InvalidXMLException;
+    boolean test(MapFilePreprocessor ctx, Element el, Node value) throws InvalidXMLException;
   }
 
   record AttributeCheck(String key, ElementPredicate pred) {
     Boolean apply(MapFilePreprocessor ctx, Element el) throws InvalidXMLException {
-      var attr = el.getAttribute(key);
+      var attr = Node.fromNullable(el.getAttribute(key));
       if (attr == null) return null;
 
-      return pred.test(ctx, el, attr.getValue());
+      return pred.test(ctx, el, attr);
     }
   }
 }
