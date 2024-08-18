29078d1000e652f5daa23fc97b61466e42e79419
Pablo Herrera <pabloherrerapalacio@gmail.com>
2024-08-19 00:51:36 +0200

    Cleanup last damage cause on reset (#1386)
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/match/MatchPlayerImpl.java
+++ core/src/main/java/tc/oc/pgm/match/MatchPlayerImpl.java
@@ -282,6 +282,7 @@ public class MatchPlayerImpl implements MatchPlayer, Comparable<MatchPlayer> {
     bukkit.setSprinting(false);
     bukkit.setFlySpeed(0.1f);
     bukkit.setWalkSpeed(WalkSpeedKit.BUKKIT_DEFAULT);
+    bukkit.setLastDamageCause(null);
     PLAYER_UTILS.clearArrowsInPlayer(bukkit);
     PLAYER_UTILS.setKnockbackReduction(bukkit, 0);
     bukkit.setVelocity(new Vector());
