bf003438ea1a7014e9fbf7ffe09fc84a93e8e5ad
Pablo Herrera <pabloherrerapalacio@gmail.com>
2024-09-16 02:03:44 +0200

    Add flexibility to voted pools (#1398)
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- util/src/main/java/tc/oc/pgm/util/math/Formula.java
+++ util/src/main/java/tc/oc/pgm/util/math/Formula.java
@@ -1,91 +1,100 @@
 package tc.oc.pgm.util.math;
 
-import java.util.Collections;
 import java.util.Set;
 import java.util.function.ToDoubleFunction;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.ExpressionContext;
 import net.objecthunter.exp4j.function.Function;
+import tc.oc.pgm.util.bukkit.BukkitUtils;
 
 public interface Formula<T> extends ToDoubleFunction<T> {
 
-  Function BOUND =
-      new Function("bound", 3) {
-        @Override
-        public double apply(double... doubles) {
-          double val = doubles[0];
-          double min = doubles[1];
-          double max = doubles[2];
-          return Math.max(min, Math.min(val, max));
-        }
-      };
+  Function BOUND = new Function("bound", 3) {
+    @Override
+    public double apply(double... doubles) {
+      double val = doubles[0];
+      double min = doubles[1];
+      double max = doubles[2];
+      return Math.max(min, Math.min(val, max));
+    }
+  };
 
-  Function RANDOM =
-      new Function("random", 0) {
-        @Override
-        public double apply(double... doubles) {
-          return Math.random();
-        }
-      };
+  Function RANDOM = new Function("random", 0) {
+    @Override
+    public double apply(double... doubles) {
+      return Math.random();
+    }
+  };
 
-  Function MAX =
-      new Function("max") {
-        @Override
-        public double apply(double... doubles) {
-          double max = doubles[0];
-          for (int i = 1; i < doubles.length; i++) max = Math.max(max, doubles[i]);
-          return max;
-        }
+  Function MAX = new Function("max") {
+    @Override
+    public double apply(double... doubles) {
+      double max = doubles[0];
+      for (int i = 1; i < doubles.length; i++) max = Math.max(max, doubles[i]);
+      return max;
+    }
 
-        @Override
-        public boolean isValidArgCount(int count) {
-          return count >= 1;
-        }
-      };
+    @Override
+    public boolean isValidArgCount(int count) {
+      return count >= 1;
+    }
+  };
 
-  Function MIN =
-      new Function("min") {
-        @Override
-        public double apply(double... doubles) {
-          double min = doubles[0];
-          for (int i = 1; i < doubles.length; i++) min = Math.min(min, doubles[i]);
-          return min;
-        }
+  Function MIN = new Function("min") {
+    @Override
+    public double apply(double... doubles) {
+      double min = doubles[0];
+      for (int i = 1; i < doubles.length; i++) min = Math.min(min, doubles[i]);
+      return min;
+    }
 
-        @Override
-        public boolean isValidArgCount(int count) {
-          return count >= 1;
-        }
-      };
+    @Override
+    public boolean isValidArgCount(int count) {
+      return count >= 1;
+    }
+  };
 
-  static <T> Formula<T> of(String expression, ContextFactory<T> context, Formula<T> fallback)
-      throws IllegalArgumentException {
+  /**
+   * Create a formula for a config, if there's a misconfiguration it logs and uses fallback
+   *
+   * @param expression The expression to parse
+   * @param variables The set of available variables in the formula
+   * @param fallback A fallback if no value is defined or parsing fails
+   * @return The formula if it parsed correctly, fallback if anything goes wrong
+   * @param <T> Type of expression context to use
+   */
+  static <T extends ExpressionContext> Formula<T> of(
+      String expression, Set<String> variables, Formula<T> fallback) {
     if (expression == null) return fallback;
 
-    return Formula.of(expression, context);
+    try {
+      return Formula.of(expression, ContextFactory.ofStatic(variables));
+    } catch (IllegalArgumentException e) {
+      BukkitUtils.getPlugin()
+          .getLogger()
+          .log(Level.SEVERE, "Failed to load formula '" + expression + "' using fallback", e);
+      return fallback;
+    }
   }
 
   static <T> Formula<T> of(String expression, ContextFactory<T> context)
       throws IllegalArgumentException {
-    Expression exp =
-        new ExpressionBuilder(expression)
-            .variables(context.getVariables())
-            .functions(BOUND, RANDOM, MAX, MIN)
-            .functions(
-                context.getArrays().stream()
-                    .map(
-                        str ->
-                            new Function(str, 1) {
-                              @Override
-                              public double apply(double... doubles) {
-                                throw new UnsupportedOperationException(
-                                    "Cannot get array value without replacement!");
-                              }
-                            })
-                    .collect(Collectors.toList()))
-            .build();
+    Expression exp = new ExpressionBuilder(expression)
+        .variables(context.getVariables())
+        .functions(BOUND, RANDOM, MAX, MIN)
+        .functions(context.getArrays().stream()
+            .map(str -> new Function(str, 1) {
+              @Override
+              public double apply(double... doubles) {
+                throw new UnsupportedOperationException(
+                    "Cannot get array value without replacement!");
+              }
+            })
+            .collect(Collectors.toList()))
+        .build();
 
     return new ExpFormula<>(exp, context);
   }
@@ -113,17 +122,10 @@ public interface Formula<T> extends ToDoubleFunction<T> {
     ExpressionContext withContext(T t);
 
     static <T extends ExpressionContext> ContextFactory<T> ofStatic(Set<String> variables) {
-      return of(variables, Collections.emptySet(), t -> t);
-    }
-
-    static <T> ContextFactory<T> of(
-        Set<String> variables,
-        Set<String> arrays,
-        java.util.function.Function<T, ExpressionContext> builder) {
-      return new ContextFactory<T>() {
+      return new ContextFactory<>() {
         @Override
         public ExpressionContext withContext(T t) {
-          return builder.apply(t);
+          return t;
         }
 
         @Override
@@ -133,7 +135,7 @@ public interface Formula<T> extends ToDoubleFunction<T> {
 
         @Override
         public Set<String> getArrays() {
-          return arrays;
+          return Set.of();
         }
       };
     }
