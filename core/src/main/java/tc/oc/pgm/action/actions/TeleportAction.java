1c576283a1844b19865b5d3b9a5817df270e2def
Pablo Herrera <pabloherrerapalacio@gmail.com>
2024-10-29 10:40:35 +0100

    Fix pitch and yaw in teleport action
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/action/actions/TeleportAction.java
+++ core/src/main/java/tc/oc/pgm/action/actions/TeleportAction.java
@@ -11,21 +11,21 @@ public class TeleportAction extends AbstractAction<MatchPlayer> {
   private final Formula<MatchPlayer> xformula;
   private final Formula<MatchPlayer> yformula;
   private final Formula<MatchPlayer> zformula;
-  private final Optional<Formula<MatchPlayer>> yawFormula;
   private final Optional<Formula<MatchPlayer>> pitchFormula;
+  private final Optional<Formula<MatchPlayer>> yawFormula;
 
   public TeleportAction(
       Formula<MatchPlayer> xformula,
       Formula<MatchPlayer> yformula,
       Formula<MatchPlayer> zformula,
-      Optional<Formula<MatchPlayer>> yawFormula,
-      Optional<Formula<MatchPlayer>> pitchFormula) {
+      Optional<Formula<MatchPlayer>> pitchFormula,
+      Optional<Formula<MatchPlayer>> yawFormula) {
     super(MatchPlayer.class);
     this.xformula = xformula;
     this.yformula = yformula;
     this.zformula = zformula;
-    this.yawFormula = yawFormula;
     this.pitchFormula = pitchFormula;
+    this.yawFormula = yawFormula;
   }
 
   @Override
@@ -34,8 +34,8 @@ public class TeleportAction extends AbstractAction<MatchPlayer> {
     location.setX(xformula.applyAsDouble(player));
     location.setY(yformula.applyAsDouble(player));
     location.setZ(zformula.applyAsDouble(player));
-    yawFormula.ifPresent((formula) -> location.setYaw((float) formula.applyAsDouble(player)));
     pitchFormula.ifPresent((formula) -> location.setPitch((float) formula.applyAsDouble(player)));
+    yawFormula.ifPresent((formula) -> location.setYaw((float) formula.applyAsDouble(player)));
     player.getBukkit().teleport(location, PlayerTeleportEvent.TeleportCause.ENDER_PEARL);
   }
 }
