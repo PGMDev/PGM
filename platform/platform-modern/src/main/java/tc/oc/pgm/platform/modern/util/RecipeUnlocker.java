a32859fa41f8336a1d5344494a78748353e7f260
Pablo Herrera <pabloherrerapalacio@gmail.com>
2024-11-24 22:28:21 +0100

    Automatically unlock recipes and warn for advancements
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

---
+++ platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/util/RecipeUnlocker.java
@@ -0,0 +1,25 @@
+package tc.oc.pgm.platform.modern.util;
+
+import net.minecraft.server.MinecraftServer;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.spigotmc.event.player.PlayerSpawnLocationEvent;
+
+public class RecipeUnlocker implements Listener {
+
+  // PlayerJoinEvent is too late as recipe init packet was already sent, so we use spawn location
+  // event
+  @EventHandler
+  public void onPlayerJoin(PlayerSpawnLocationEvent event) {
+    // Non-joined players haven't been added to the player list yet
+    if (Bukkit.getPlayer(event.getPlayer().getUniqueId()) != null) return;
+
+    var player = ((CraftPlayer) event.getPlayer()).getHandle();
+    player
+        .getRecipeBook()
+        .known
+        .addAll(MinecraftServer.getServer().getRecipeManager().byName.keySet());
+  }
+}
