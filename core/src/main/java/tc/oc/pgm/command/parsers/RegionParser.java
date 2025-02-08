b300c7d265f1b6bc805751579aab85ea86174479
Pablo Herrera <pabloherrerapalacio@gmail.com>
2025-02-08 05:22:43 +0100

    Cleanup region parser
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/command/parsers/RegionParser.java
+++ core/src/main/java/tc/oc/pgm/command/parsers/RegionParser.java
@@ -5,35 +5,33 @@ import java.util.Map;
 import org.bukkit.command.CommandSender;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.parser.ParserParameters;
+import tc.oc.pgm.api.feature.FeatureDefinition;
 import tc.oc.pgm.api.region.Region;
 import tc.oc.pgm.features.FeatureDefinitionContext;
 import tc.oc.pgm.filters.FilterMatchModule;
 
 public final class RegionParser
-    extends MatchObjectParser<Region, Map.Entry<String, Region>, FilterMatchModule> {
+    extends MatchObjectParser<Region, Map.Entry<String, FeatureDefinition>, FilterMatchModule> {
 
   public RegionParser(CommandManager<CommandSender> manager, ParserParameters options) {
     super(manager, options, Region.class, FilterMatchModule.class, "regions");
   }
 
   @Override
-  @SuppressWarnings("unchecked")
-  protected Iterable<Map.Entry<String, Region>> objects(FilterMatchModule module) {
+  protected Iterable<Map.Entry<String, FeatureDefinition>> objects(FilterMatchModule module) {
     if (!(module.getFilterContext() instanceof FeatureDefinitionContext fdc)) return List.of();
-
     return () -> fdc.stream()
         .filter(entry -> entry != null && entry.getValue() instanceof Region)
-        .map(entry -> (Map.Entry<String, Region>) (Map.Entry<?, ?>) entry)
         .iterator();
   }
 
   @Override
-  protected String getName(Map.Entry<String, Region> obj) {
+  protected String getName(Map.Entry<String, FeatureDefinition> obj) {
     return obj.getKey();
   }
 
   @Override
-  protected Region getValue(Map.Entry<String, Region> obj) {
-    return obj.getValue();
+  protected Region getValue(Map.Entry<String, FeatureDefinition> obj) {
+    return (Region) obj.getValue();
   }
 }
