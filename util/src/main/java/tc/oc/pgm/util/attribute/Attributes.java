f740d8a5a002831240183025dfe12e3f1cbcc6cd
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2025-05-31 18:39:01 -0700

    Cleanup attributes work
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- util/src/main/java/tc/oc/pgm/util/attribute/Attributes.java
+++ util/src/main/java/tc/oc/pgm/util/attribute/Attributes.java
@@ -15,30 +15,28 @@ public class Attributes {
       new HashMap<>(ATTRIBUTE_UTILS.getAttributeValues().length);
 
   static {
+    parse("GENERIC_MAX_HEALTH", "MAX_HEALTH");
+    parse("GENERIC_FOLLOW_RANGE", "FOLLOW_RANGE");
+    parse("GENERIC_MOVEMENT_SPEED", "MOVEMENT_SPEED");
+    parse("GENERIC_ATTACK_DAMAGE", "ATTACK_DAMAGE");
+    parse("HORSE_JUMP_STRENGTH", "GENERIC_JUMP_STRENGTH", "JUMP_STRENGTH");
+    parse("ZOMBIE_SPAWN_REINFORCEMENTS", "SPAWN_REINFORCEMENTS");
+    if (Platform.isModern()) {
+      parse("GENERIC_FLYING_SPEED", "FLYING_SPEED");
+      parse("GENERIC_ATTACK_SPEED", "ATTACK_SPEED");
+      parse("GENERIC_ARMOR", "ARMOR");
+      parse("GENERIC_ARMOR_TOUGHNESS", "ARMOR_TOUGHNESS");
+      parse("GENERIC_LUCK", "LUCK");
+    }
+
     for (Attribute value : ATTRIBUTE_UTILS.getAttributeValues()) {
       if (value != null)
         BY_NAME.put(StringUtils.simplify(ATTRIBUTE_UTILS.getAttributeName(value)), value);
     }
   }
 
-  // Only define attributes which existed <1.13 as that was the highest version ProjectAres servers
-  // used. However, only parse 1.9+ attributes on modern servers.
-  public static Attribute MAX_HEALTH = parse("GENERIC_MAX_HEALTH", "max_health");
-  public static Attribute FOLLOW_RANGE = parse("GENERIC_FOLLOW_RANGE", "follow_range");
   public static Attribute KNOCKBACK_RESISTANCE =
-      parse("GENERIC_KNOCKBACK_RESISTANCE", "knockback_resistance");
-  public static Attribute MOVEMENT_SPEED = parse("GENERIC_MOVEMENT_SPEED", "movement_speed");
-  public static Attribute FLYING_SPEED = parse(true, "GENERIC_FLYING_SPEED", "flying_speed");
-  public static Attribute ATTACK_DAMAGE = parse("GENERIC_ATTACK_DAMAGE", "attack_damage");
-  public static Attribute ATTACK_SPEED = parse(true, "GENERIC_ATTACK_SPEED", "attack_speed");
-  public static Attribute ARMOR = parse(true, "GENERIC_ARMOR", "armor");
-  public static Attribute ARMOR_TOUGHNESS =
-      parse(true, "GENERIC_ARMOR_TOUGHNESS", "armor_toughness");
-  public static Attribute LUCK = parse(true, "GENERIC_LUCK", "luck");
-  public static Attribute JUMP_STRENGTH =
-      parse("HORSE_JUMP_STRENGTH", "GENERIC_JUMP_STRENGTH", "jump_strength");
-  public static Attribute SPAWN_REINFORCEMENTS =
-      parse("ZOMBIE_SPAWN_REINFORCEMENTS", "spawn_reinforcements");
+      parse("GENERIC_KNOCKBACK_RESISTANCE", "KNOCKBACK_RESISTANCE");
 
   private static Attribute parse(String... names) {
     Attribute type = BukkitUtils.parse(ATTRIBUTE_UTILS::getAttributeValue, names);
@@ -46,11 +44,6 @@ public class Attributes {
       BY_NAME.put(StringUtils.simplify(name), type);
     }
     return type;
-  }
-
-  private static Attribute parse(boolean modernAttribute, String... names) {
-    if (modernAttribute && Platform.isModern()) return parse(names);
-    else return null;
   }
 
   public static Attribute getByName(String name) {
