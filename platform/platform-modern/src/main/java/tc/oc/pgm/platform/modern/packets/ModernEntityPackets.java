e72df98ca6545b5c95f43efa00dc22ce8f8377d2
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2025-06-03 11:56:42 -0700

    Fixup teleportEntityPacket on modern
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/packets/ModernEntityPackets.java
+++ platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/packets/ModernEntityPackets.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.protocol.game.ClientboundBundlePacket;
+import net.minecraft.network.protocol.game.ClientboundEntityPositionSyncPacket;
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket;
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
 import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket;
@@ -19,7 +20,9 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.PositionMoveRotation;
 import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.phys.Vec3;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
@@ -85,16 +88,12 @@ public class ModernEntityPackets implements EntityPackets {
 
   @Override
   public Packet teleportEntityPacket(int entityId, Location location) {
-    PacketContainer packet = PlPacket.PL.createPacket(PacketType.Play.Server.REL_ENTITY_MOVE_LOOK);
+    Vec3 position = new Vec3(location.getX(), location.getY(), location.getZ());
+    PositionMoveRotation positionMoveRotation =
+        new PositionMoveRotation(position, Vec3.ZERO, location.getYaw(), location.getPitch());
 
-    packet.getIntegers().write(0, entityId);
-    packet.getShorts().write(0, (short) location.getX());
-    packet.getShorts().write(1, (short) location.getY());
-    packet.getShorts().write(2, (short) location.getZ());
-    packet.getBytes().write(0, (byte) (location.getYaw() * 256 / 360));
-    packet.getBytes().write(1, (byte) (location.getPitch() * 256 / 360));
-
-    return new PlPacket(packet);
+    return new ModernPacket<>(
+        new ClientboundEntityPositionSyncPacket(entityId, positionMoveRotation, false));
   }
 
   @Override
