a749a009848bc754e187f9af8ec63257e145a586
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2025-07-19 14:21:13 -0700

    Use 1.21.8 intersect jar
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    compileOnly("dev.pgm.paper:paper-api:1.8_1.21.7-SNAPSHOT")
+    compileOnly("dev.pgm.paper:paper-api:1.8_1.21.8-SNAPSHOT")
 
     implementation(project(":util"))
     runtimeOnly(project(":platform-sportpaper")) { exclude("*") }
