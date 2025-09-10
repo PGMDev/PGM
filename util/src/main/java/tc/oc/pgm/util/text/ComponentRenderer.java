c48c710df3765515a78552176f29eed6a565f619
Pablo Herrera <pabloherrerapalacio@gmail.com>
2025-09-10 22:41:53 +0200

    Fix virtual component rendering
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- util/src/main/java/tc/oc/pgm/util/text/ComponentRenderer.java
+++ util/src/main/java/tc/oc/pgm/util/text/ComponentRenderer.java
@@ -3,11 +3,15 @@ package tc.oc.pgm.util.text;
 import static net.kyori.adventure.text.Component.text;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.pointer.Pointered;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.TranslationArgument;
 import net.kyori.adventure.text.VirtualComponent;
 import net.kyori.adventure.text.VirtualComponentRenderer;
 import net.kyori.adventure.text.renderer.TranslatableComponentRenderer;
@@ -56,5 +60,28 @@ public class ComponentRenderer extends TranslatableComponentRenderer<Pointered> 
   @Override
   protected @Nullable MessageFormat translate(@NotNull String key, @NotNull Pointered context) {
     return GlobalTranslator.translator().translate(key, TextTranslations.getLocale(context));
+  }
+
+  /**
+   * Reimplementation of TranslatableComponentRenderer#renderTranslatable to allow virtual
+   * components rendering.<br>
+   * See <a href="https://github.com/KyoriPowered/adventure/issues/1299">Adventure#1299</a>.
+   */
+  @Override
+  protected @NotNull Component renderTranslatable(
+      @NotNull TranslatableComponent component, final @NotNull Pointered context) {
+    final List<TranslationArgument> arguments = component.arguments();
+
+    if (!arguments.isEmpty()) {
+      final List<TranslationArgument> translatedArguments = new ArrayList<>(arguments);
+      for (int i = 0; i < arguments.size(); i++) {
+        if (arguments.get(i).value() instanceof Component c)
+          translatedArguments.set(i, TranslationArgument.component(this.render(c, context)));
+      }
+
+      component = component.toBuilder().arguments(translatedArguments).build();
+    }
+
+    return this.renderTranslatableInner(component, context);
   }
 }
