7912dc6d9804dc9550d52347296a34801fadc319
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2025-09-12 01:09:06 -0700

    Update Adventure (#1550)
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- buildSrc/src/main/kotlin/buildlogic.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.java-conventions.gradle.kts
@@ -23,11 +23,9 @@ repositories {
 
 dependencies {
     api("org.jdom:jdom2:2.0.6.1")
-    api("net.kyori:adventure-api:4.23.0")
-    api("net.kyori:adventure-text-serializer-plain:4.23.0")
-    // Use jitpack for specific snapshot for Paper 1.21.6+ support
-    // api("net.kyori:adventure-platform-bukkit:4.4.0")
-    api("com.github.KyoriPowered:adventure-platform:5867a1939a")
+    api("net.kyori:adventure-api:4.24.0")
+    api("net.kyori:adventure-text-serializer-plain:4.24.0")
+    api("net.kyori:adventure-platform-bukkit:4.4.1")
     api("org.incendo:cloud-core:2.0.0")
     api("org.incendo:cloud-annotations:2.0.0")
     api("org.incendo:cloud-paper:2.0.0-beta.11")
