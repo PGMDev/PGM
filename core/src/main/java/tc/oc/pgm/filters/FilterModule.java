034472e9508adb53ed7aaad8c71dd1fc1430927d
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2025-09-15 09:52:53 -0700

    Comment out breaking change
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/filters/FilterModule.java
+++ core/src/main/java/tc/oc/pgm/filters/FilterModule.java
@@ -18,7 +18,6 @@ import tc.oc.pgm.filters.matcher.block.VoidFilter;
 import tc.oc.pgm.filters.matcher.match.MatchPhaseFilter;
 import tc.oc.pgm.filters.matcher.player.CanFlyFilter;
 import tc.oc.pgm.filters.matcher.player.FlyingFilter;
-import tc.oc.pgm.filters.matcher.player.GlidingFilter;
 import tc.oc.pgm.filters.matcher.player.GroundedFilter;
 import tc.oc.pgm.filters.matcher.player.ParticipatingFilter;
 import tc.oc.pgm.filters.matcher.player.PlayerMovementFilter;
@@ -96,7 +95,8 @@ public class FilterModule implements MapModule<FilterMatchModule> {
         features.addFeature(null, "sprinting", PlayerMovementFilter.SPRINTING);
         features.addFeature(null, "grounded", GroundedFilter.INSTANCE);
         features.addFeature(null, "flying", FlyingFilter.INSTANCE);
-        features.addFeature(null, "gliding", GlidingFilter.INSTANCE);
+        // TODO: support fallback feature ids being overriden without being a breaking change
+        // features.addFeature(null, "gliding", GlidingFilter.INSTANCE);
         features.addFeature(null, "can-fly", CanFlyFilter.INSTANCE);
       }
 
