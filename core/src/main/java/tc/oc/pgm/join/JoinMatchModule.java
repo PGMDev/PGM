f95ca2898f91269099aed289deec37ec68230a70
slayersource <slayersource1@gmail.com>
2025-10-25 23:39:54 -0700

    Remove line
    Signed-off-by: slayersource <slayersource1@gmail.com>

--- core/src/main/java/tc/oc/pgm/join/JoinMatchModule.java
+++ core/src/main/java/tc/oc/pgm/join/JoinMatchModule.java
@@ -170,7 +170,6 @@ public class JoinMatchModule implements MatchModule, Listener, JoinHandler {
       return false;
     }
 
-
     if (leaving.getParty() instanceof ObserverParty) {
       leaving.sendWarning(
           translatable("join.err.alreadyJoined.team", leaving.getParty().getName()));
