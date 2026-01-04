0c646af2329b897f896ddbe55a995e34cb64b1c6
Christopher White <18whitechristop@gmail.com>
2026-01-04 11:33:01 -0800

    Make player class components more interactive
    Signed-off-by: Christopher White <18whitechristop@gmail.com>

--- core/src/main/java/tc/oc/pgm/classes/PlayerClass.java
+++ core/src/main/java/tc/oc/pgm/classes/PlayerClass.java
@@ -1,9 +1,15 @@
 package tc.oc.pgm.classes;
 
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.event.HoverEvent.showText;
 import static tc.oc.pgm.util.Assert.assertNotNull;
 
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 import tc.oc.pgm.kits.Kit;
@@ -52,6 +58,19 @@ public class PlayerClass {
 
   public @Nullable String getLongDescription() {
     return this.longdescription;
+  }
+
+  public Component getComponent() {
+    TextComponent.Builder component =
+        text().content(this.name).color(NamedTextColor.GOLD).decoration(TextDecoration.BOLD, true);
+
+    String desc = this.description != null ? this.description : this.longdescription;
+    if (desc != null) {
+      component.hoverEvent(showText(
+          text(this.name + ": ", NamedTextColor.GOLD).append(text(desc, NamedTextColor.GRAY))));
+    }
+
+    return component.build();
   }
 
   public boolean isSticky() {
