d013ce139ab542372e71407279609f780701c3eb
Pablo Herrera <pabloherrerapalacio@gmail.com>
2026-01-24 03:54:28 +0100

    Fix some issues with team names (#1601)
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/action/actions/TeamAliasAction.java
+++ core/src/main/java/tc/oc/pgm/action/actions/TeamAliasAction.java
@@ -1,6 +1,10 @@
 package tc.oc.pgm.action.actions;
 
+import java.util.logging.Level;
+import tc.oc.pgm.api.PGM;
 import tc.oc.pgm.api.party.Party;
+import tc.oc.pgm.teams.Team;
+import tc.oc.pgm.teams.TeamMatchModule;
 
 public class TeamAliasAction extends AbstractAction<Party> {
   protected final String alias;
@@ -12,6 +16,18 @@ public class TeamAliasAction extends AbstractAction<Party> {
 
   @Override
   public void trigger(Party party) {
+    // No-op for non-team parties, we don't want people renaming obs
+    if (!(party instanceof Team)) return;
+    var existingTeam = party.getMatch().needModule(TeamMatchModule.class).getTeam(alias);
+    if (existingTeam != null && existingTeam != party) {
+      PGM.get()
+          .getGameLogger()
+          .log(
+              Level.WARNING,
+              "Team alias '%s' cannot be applied to '%s' as it's already in use"
+                  .formatted(alias, party.getName()));
+      return;
+    }
     party.setName(this.alias);
   }
 }
