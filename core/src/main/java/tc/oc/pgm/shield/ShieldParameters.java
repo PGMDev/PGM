072234c8415cc07ffef7d7a5a3e9160dfae25b70
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2026-02-04 17:40:00 -0800

    ShieldParameters
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/shield/ShieldParameters.java
+++ core/src/main/java/tc/oc/pgm/shield/ShieldParameters.java
@@ -2,16 +2,8 @@ package tc.oc.pgm.shield;
 
 import java.time.Duration;
 
-public class ShieldParameters {
+public record ShieldParameters(double maxHealth, Duration rechargeDelay) {
 
   public static final double DEFAULT_HEALTH = 4d;
   public static final Duration DEFAULT_DELAY = Duration.ofSeconds(4);
-
-  public final double maxHealth;
-  public final Duration rechargeDelay;
-
-  public ShieldParameters(double maxHealth, Duration rechargeDelay) {
-    this.maxHealth = maxHealth;
-    this.rechargeDelay = rechargeDelay;
-  }
 }
