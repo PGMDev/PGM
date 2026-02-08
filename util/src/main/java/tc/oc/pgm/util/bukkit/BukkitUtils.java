9e01a63023d04724d7d46bf1fba5361c521f8878
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2026-02-07 16:12:12 -0800

    misc: use requireNonNullElse in BukkitUtils
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- util/src/main/java/tc/oc/pgm/util/bukkit/BukkitUtils.java
+++ util/src/main/java/tc/oc/pgm/util/bukkit/BukkitUtils.java
@@ -3,6 +3,7 @@ package tc.oc.pgm.util.bukkit;
 import com.google.common.collect.ImmutableMap;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import org.bukkit.Bukkit;
@@ -34,20 +35,12 @@ public interface BukkitUtils {
 
   static ChatColor dyeColorToChatColor(DyeColor dyeColor) {
     ChatColor chatColor = DYE_CHAT_MAP.get(dyeColor);
-    if (chatColor != null) {
-      return chatColor;
-    } else {
-      return ChatColor.WHITE;
-    }
+    return Objects.requireNonNullElse(chatColor, ChatColor.WHITE);
   }
 
   static DyeColor chatColorToDyeColor(ChatColor chatColor) {
     DyeColor dyeColor = CHAT_DYE_MAP.get(chatColor);
-    if (dyeColor != null) {
-      return dyeColor;
-    } else {
-      return DyeColor.WHITE;
-    }
+    return Objects.requireNonNullElse(dyeColor, DyeColor.WHITE);
   }
 
   Map<DyeColor, ChatColor> DYE_CHAT_MAP = ImmutableMap.<DyeColor, ChatColor>builder()
