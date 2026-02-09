PGMDev/PGM

commit 2651562b965f17c90d3face2052db58baffde1dd

Fix inventory access bugs in modern (#1610)
Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

core/src/main/java/tc/oc/pgm/kits/ArmorKit.java
@@ -1,12 +1,14 @@
 package tc.oc.pgm.kits;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.kits.tag.ItemModifier;
 import tc.oc.pgm.util.inventory.ArmorType;
+import tc.oc.pgm.util.inventory.Slot;
 
 public class ArmorKit extends AbstractKit {
   public static class ArmorItem {
@@ -19,14 +21,21 @@ public class ArmorKit extends AbstractKit {
     }
   }
 
-  private final Map<ArmorType, ArmorItem> armor;
+  private final Map<Slot.Armor, ArmorItem> armor;
 
-  public ArmorKit(Map<ArmorType, ArmorItem> armor) {
+  public ArmorKit(Map<Slot.Armor, ArmorItem> armor) {
     this.armor = armor;
   }
 
+  @Deprecated(forRemoval = true)
   public Map<ArmorType, ArmorItem> getArmor() {
-    return armor;
+    var remapped = new HashMap<ArmorType, ArmorItem>();
+    armor.forEach((slot, armorItem) -> remapped.put(slot.getArmorType(), armorItem));
+    return remapped;
+  }
+
+  public Collection<ArmorItem> getArmorItems() {
+    return armor.values();
   }
 
   /**
@@ -35,14 +44,12 @@ public class ArmorKit extends AbstractKit {
    */
   @Override
   public void applyPostEvent(MatchPlayer player, boolean force, List<ItemStack> displacedItems) {
-    ItemStack[] wearing = player.getBukkit().getInventory().getArmorContents();
-    for (Map.Entry<ArmorType, ArmorItem> entry : this.armor.entrySet()) {
-      int slot = entry.getKey().ordinal();
-      if (force || wearing[slot] == null || wearing[slot].getType() == Material.AIR) {
-        wearing[slot] = entry.getValue().stack.clone();
-        ItemModifier.apply(wearing[slot], player);
+    this.armor.forEach((slot, item) -> {
+      var wearing = slot.getItem(player);
+      if (force || wearing == null) {
+        slot.setItem(player, wearing = item.stack.clone());
+        ItemModifier.apply(wearing, player);
       }
-    }
-    player.getBukkit().getInventory().setArmorContents(wearing);
+    });
   }
 }

core/src/main/java/tc/oc/pgm/action/actions/EnchantItemAction.java
@@ -6,6 +6,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.util.inventory.ItemMatcher;
+import tc.oc.pgm.util.inventory.Slot;
 import tc.oc.pgm.util.math.Formula;
 
 public class EnchantItemAction extends AbstractAction<MatchPlayer> {
@@ -27,23 +28,16 @@ public class EnchantItemAction extends AbstractAction<MatchPlayer> {
 
     int level = Math.max(0, (int) this.level.applyAsDouble(player));
 
-    for (ItemStack current : inv.getArmorContents()) {
-      if (current != null && matcher.matches(current)) enchant(current, level);
-    }
-    for (int i = 0; i < inv.getSize(); i++) {
-      ItemStack current = inv.getItem(i);
-      if (current != null && matcher.matches(current)) {
-        enchant(current, level);
-        // Makes item sync with client instantly
-        inv.setItem(i, current);
-      }
-    }
+    Slot.Player.forEach(inv, (slot, stack) -> {
+      if (matcher.matches(stack)) slot.setItem(inv, enchant(stack, level));
+    });
   }
 
-  private void enchant(ItemStack current, int level) {
+  private ItemStack enchant(ItemStack current, int level) {
     if (current.getEnchantmentLevel(enchant) != level) {
       if (level == 0) current.removeEnchantment(enchant);
       else current.addUnsafeEnchantment(enchant, level);
     }
+    return current;
   }
 }

core/src/main/java/tc/oc/pgm/action/actions/ReplaceItemAction.java
@@ -5,6 +5,7 @@ import org.bukkit.inventory.PlayerInventory;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.kits.tag.ItemModifier;
 import tc.oc.pgm.util.inventory.ItemMatcher;
+import tc.oc.pgm.util.inventory.Slot;
 
 public class ReplaceItemAction extends AbstractAction<MatchPlayer> {
 
@@ -25,20 +26,9 @@ public class ReplaceItemAction extends AbstractAction<MatchPlayer> {
   @Override
   public void trigger(MatchPlayer player) {
     PlayerInventory inv = player.getInventory();
-
-    ItemStack[] armor = inv.getArmorContents();
-    for (int i = 0; i < armor.length; i++) {
-      ItemStack current = armor[i];
-      if (current == null || !matcher.matches(current)) continue;
-      armor[i] = replaceItem(current, player);
-    }
-    inv.setArmorContents(armor);
-
-    for (int i = 0; i < inv.getSize(); i++) {
-      ItemStack current = inv.getItem(i);
-      if (current == null || !matcher.matches(current)) continue;
-      inv.setItem(i, replaceItem(current, player));
-    }
+    Slot.Player.forEach(inv, (slot, stack) -> {
+      if (matcher.matches(stack)) slot.setItem(inv, replaceItem(stack, player));
+    });
   }
 
   private ItemStack replaceItem(ItemStack current, MatchPlayer player) {

core/src/main/java/tc/oc/pgm/filters/matcher/player/CarryingItemFilter.java
@@ -18,6 +18,7 @@ import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.kits.ApplyKitEvent;
 import tc.oc.pgm.util.event.PlayerItemTransferEvent;
 import tc.oc.pgm.util.inventory.ItemMatcher;
+import tc.oc.pgm.util.inventory.Slot;
 
 public class CarryingItemFilter extends ParticipantItemFilter {
   public CarryingItemFilter(ItemMatcher matcher) {
@@ -39,9 +40,7 @@ public class CarryingItemFilter extends ParticipantItemFilter {
 
   @Override
   protected Stream<ItemStack> getItems(MatchPlayer player) {
-    Stream<ItemStack> inventory = Stream.concat(
-        Arrays.stream(player.getBukkit().getInventory().getContents()),
-        Stream.of(player.getBukkit().getItemOnCursor()));
+    Stream<ItemStack> inventory = Slot.Player.player().map(s -> s.getItem(player));
 
     // Potentially add the crafting grid if that's the currently open inventory
     InventoryView invView = player.getBukkit().getOpenInventory();

core/src/main/java/tc/oc/pgm/filters/matcher/player/HoldingItemFilter.java
@@ -15,6 +15,7 @@ import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.kits.ApplyKitEvent;
 import tc.oc.pgm.util.event.PlayerItemTransferEvent;
 import tc.oc.pgm.util.inventory.ItemMatcher;
+import tc.oc.pgm.util.inventory.Slot;
 
 public class HoldingItemFilter extends ParticipantItemFilter {
   public HoldingItemFilter(ItemMatcher matcher) {
@@ -36,6 +37,6 @@ public class HoldingItemFilter extends ParticipantItemFilter {
 
   @Override
   protected Stream<ItemStack> getItems(MatchPlayer player) {
-    return Stream.of(player.getBukkit().getItemInHand());
+    return Slot.Equipment.hands().map(s -> s.getItem(player));
   }
 }

core/src/main/java/tc/oc/pgm/filters/matcher/player/WearingItemFilter.java
@@ -1,7 +1,6 @@
 package tc.oc.pgm.filters.matcher.player;
 
 import com.google.common.collect.ImmutableList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Stream;
 import org.bukkit.event.Event;
@@ -13,6 +12,7 @@ import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.kits.ApplyKitEvent;
 import tc.oc.pgm.util.event.PlayerItemTransferEvent;
 import tc.oc.pgm.util.inventory.ItemMatcher;
+import tc.oc.pgm.util.inventory.Slot;
 
 public class WearingItemFilter extends ParticipantItemFilter {
   public WearingItemFilter(ItemMatcher matcher) {
@@ -31,6 +31,6 @@ public class WearingItemFilter extends ParticipantItemFilter {
 
   @Override
   protected Stream<ItemStack> getItems(MatchPlayer player) {
-    return Arrays.stream(player.getBukkit().getInventory().getArmorContents());
+    return Slot.Armor.armor().map(s -> s.getItem(player));
   }
 }

core/src/main/java/tc/oc/pgm/kits/ItemKit.java
@@ -80,7 +80,7 @@ public class ItemKit implements KitDefinition {
 
     if (force) {
       for (Entry<Slot, ItemStack> kitEntry : event.getSlotItems().entrySet()) {
-        kitEntry.getKey().putItem(holder, kitEntry.getValue().clone());
+        kitEntry.getKey().setItem(holder, kitEntry.getValue().clone());
       }
     } else {
       // Tools in the player's inv are repaired using matching tools in the kit with less damage
@@ -144,7 +144,7 @@ public class ItemKit implements KitDefinition {
 
         Slot kitSlot = kitEntry.getKey();
         if (InventoryUtils.isNothing(kitSlot.getItem(holder))) {
-          kitSlot.putItem(holder, kitStack);
+          kitSlot.setItem(holder, kitStack);
         } else {
           displacedItems.add(kitStack);
         }

core/src/main/java/tc/oc/pgm/kits/KitModule.java
@@ -121,8 +121,8 @@ public class KitModule implements MapModule<KitMatchModule> {
       }
 
       if (imm != null) {
-        if (kit instanceof ArmorKit) {
-          for (ArmorKit.ArmorItem armor : ((ArmorKit) kit).getArmor().values()) {
+        if (kit instanceof ArmorKit ak) {
+          for (ArmorKit.ArmorItem armor : ak.getArmorItems()) {
             imm.applyRules(armor.stack);
           }
         }

commit 03b8f44508d140f1241e0e32f0d0c28cdb30e442

fix(kits): correct item modifier application order
Commit 2651562b965f17c90d3face2052db58baffde1dd introduced a regression with armor kits making item modifiers such as `team-color` not applied. This patch corrects the application order, fixing the issue.

Signed-off-by: TTtie <me@tttie.cz>

core/src/main/java/tc/oc/pgm/kits/ArmorKit.java
@@ -47,8 +47,8 @@ public class ArmorKit extends AbstractKit {
     this.armor.forEach((slot, item) -> {
       var wearing = slot.getItem(player);
       if (force || wearing == null) {
-        slot.setItem(player, wearing = item.stack.clone());
-        ItemModifier.apply(wearing, player);
+        ItemModifier.apply(wearing = item.stack.clone(), player);
+        slot.setItem(player, wearing);
       }
     });
   }