6d3c2f8057965afe28c7658ff3077d84cbed6e6d
Pablo Herrera <pabloherrerapalacio@gmail.com>
2026-04-15 00:31:35 +0200

    Implement multiple consumables
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/inventory/ModernItemTagFactory.java
+++ platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/inventory/ModernItemTagFactory.java
@@ -2,6 +2,7 @@ package tc.oc.pgm.platform.modern.inventory;
 
 import static tc.oc.pgm.util.platform.Supports.Variant.PAPER;
 
+import java.util.List;
 import org.bukkit.persistence.PersistentDataType;
 import tc.oc.pgm.util.inventory.tag.ItemTag;
 import tc.oc.pgm.util.platform.Supports;
@@ -12,6 +13,11 @@ public class ModernItemTagFactory implements ItemTag.Factory {
   @Override
   public ItemTag<String> newString(String key) {
     return new ModernItemTag<>(key, PersistentDataType.STRING);
+  }
+
+  @Override
+  public ItemTag<List<String>> newStringList(String key) {
+    return new ModernItemTag<>(key, PersistentDataType.LIST.strings());
   }
 
   @Override
--- platform/platform-sportpaper/src/main/java/tc/oc/pgm/platform/sportpaper/inventory/SpItemTagFactory.java
+++ platform/platform-sportpaper/src/main/java/tc/oc/pgm/platform/sportpaper/inventory/SpItemTagFactory.java
@@ -1,5 +1,6 @@
 package tc.oc.pgm.platform.sportpaper.inventory;
 
+import java.util.List;
 import tc.oc.pgm.util.inventory.tag.ItemTag;
 import tc.oc.pgm.util.platform.Supports;
 
@@ -8,6 +9,11 @@ public class SpItemTagFactory implements ItemTag.Factory {
   @Override
   public ItemTag<String> newString(String key) {
     return new SpItemTag<>(key, SpItemTag.Codec.STRING);
+  }
+
+  @Override
+  public ItemTag<List<String>> newStringList(String key) {
+    return new SpItemTag<>(key, SpItemTag.Codec.STRING_LIST);
   }
 
   @Override
