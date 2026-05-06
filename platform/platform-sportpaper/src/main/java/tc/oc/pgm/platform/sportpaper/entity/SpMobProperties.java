eb7ee5891fb33fddcf8ddb4ad495e4c1bffce741
Christopher White <18whitechristop@gmail.com>
2026-05-05 21:57:48 -0700

    Add support for Spawning LivingEntities to xml spawners
    Signed-off-by: Christopher White <18whitechristop@gmail.com>

---
+++ platform/platform-sportpaper/src/main/java/tc/oc/pgm/platform/sportpaper/entity/SpMobProperties.java
@@ -0,0 +1,23 @@
+package tc.oc.pgm.platform.sportpaper.entity;
+
+import static tc.oc.pgm.util.platform.Supports.Variant.SPORTPAPER;
+
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Skeleton;
+import tc.oc.pgm.entity.MobProperties;
+import tc.oc.pgm.util.platform.Supports;
+
+@Supports(SPORTPAPER)
+public class SpMobProperties extends MobProperties {
+
+  public SpMobProperties() {
+    register(ArmorStand.class, "gravity", BOOL, ArmorStand::setGravity);
+    register(Horse.class, "variant", enumOf(Horse.Variant.class), Horse::setVariant);
+    register(
+        Skeleton.class,
+        "skeleton-type",
+        enumOf(Skeleton.SkeletonType.class),
+        Skeleton::setSkeletonType);
+  }
+}
