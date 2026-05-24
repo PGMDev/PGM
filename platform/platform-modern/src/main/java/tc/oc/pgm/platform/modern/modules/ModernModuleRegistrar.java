54949a1bc0763b409cc255e145951dfe425b534a
BT <43831917+calcastor@users.noreply.github.com>
2026-05-24 13:44:49 -0700

    Implement a tracker for end crystals (#1675)
    Signed-off-by: BT <43831917+calcastor@users.noreply.github.com>

--- platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/modules/ModernModuleRegistrar.java
+++ platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/modules/ModernModuleRegistrar.java
@@ -4,6 +4,7 @@ import static tc.oc.pgm.util.platform.Supports.Variant.PAPER;
 
 import tc.oc.pgm.api.Modules;
 import tc.oc.pgm.platform.modern.modules.kits.ModernKitMatchModule;
+import tc.oc.pgm.platform.modern.modules.tracker.ModernTrackerMatchModule;
 import tc.oc.pgm.platform.modern.modules.trim.TrimMatchModule;
 import tc.oc.pgm.platform.modern.modules.trim.TrimModule;
 import tc.oc.pgm.platform.modern.modules.waypoint.WaypointMatchModule;
@@ -17,6 +18,7 @@ public class ModernModuleRegistrar implements Modules.ModuleRegistrar {
         ModernEventFilterMatchModule.class, new ModernEventFilterMatchModule.Factory());
     modules.register(ModernKitMatchModule.class, new ModernKitMatchModule.Factory());
     modules.register(ModernMobsMatchModule.class, new ModernMobsMatchModule.Factory());
+    modules.register(ModernTrackerMatchModule.class, new ModernTrackerMatchModule.Factory());
     modules.register(TrimModule.class, TrimMatchModule.class, new TrimModule.Factory());
     modules.register(WaypointMatchModule.class, WaypointMatchModule::new);
   }
