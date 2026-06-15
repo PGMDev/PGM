5442b35c38267fe0c036fb802e6896865d2a464f
Pablo Herrera <pabloherrerapalacio@gmail.com>
2026-06-15 10:02:49 +0200

    Add flexibility to projectile entities (#1689)
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- core/src/main/java/tc/oc/pgm/projectile/ProjectileDefinition.java
+++ core/src/main/java/tc/oc/pgm/projectile/ProjectileDefinition.java
@@ -2,54 +2,44 @@ package tc.oc.pgm.projectile;
 
 import java.time.Duration;
 import java.util.List;
-import org.bukkit.entity.Entity;
 import org.bukkit.potion.PotionEffect;
 import org.jetbrains.annotations.Nullable;
 import tc.oc.pgm.api.filter.Filter;
 import tc.oc.pgm.features.SelfIdentifyingFeatureDefinition;
-import tc.oc.pgm.util.material.BlockMaterialData;
+import tc.oc.pgm.projectile.projectiles.PgmProjectile;
 
 public class ProjectileDefinition extends SelfIdentifyingFeatureDefinition {
-  protected @Nullable String name;
-  protected @Nullable Double damage;
-  protected @Nullable Float power;
-  protected double velocity;
-  protected ClickAction clickAction;
-  protected Class<? extends Entity> projectile;
-  protected List<PotionEffect> potion;
-  protected Filter destroyFilter;
-  protected Duration coolDown;
-  protected boolean throwable;
-  protected boolean precise;
-  protected BlockMaterialData blockMaterial;
+  protected final @Nullable String name;
+  protected final @Nullable Double damage;
+  protected final double velocity;
+  protected final ClickAction clickAction;
+  protected final PgmProjectile projectile;
+  protected final List<PotionEffect> potion;
+  protected final Filter destroyFilter;
+  protected final Duration coolDown;
+  protected final boolean throwable;
 
   public ProjectileDefinition(
       @Nullable String id,
       @Nullable String name,
       @Nullable Double damage,
-      @Nullable Float power,
       double velocity,
       ClickAction clickAction,
-      Class<? extends Entity> entity,
+      PgmProjectile projectile,
       List<PotionEffect> potion,
       Filter destroyFilter,
       Duration coolDown,
-      boolean throwable,
-      boolean precise,
-      BlockMaterialData blockMaterial) {
+      boolean throwable) {
     super(id);
     this.name = name;
     this.damage = damage;
-    this.power = power;
     this.velocity = velocity;
     this.clickAction = clickAction;
-    this.projectile = entity;
+    this.projectile = projectile;
     this.potion = potion;
     this.destroyFilter = destroyFilter;
     this.coolDown = coolDown;
     this.throwable = throwable;
-    this.precise = precise;
-    this.blockMaterial = blockMaterial;
   }
 
   public @Nullable String getName() {
